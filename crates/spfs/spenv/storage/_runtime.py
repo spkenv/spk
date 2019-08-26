3fa1d6034e390ea22c23182360effd076f79b0ae
Ryan Bottriell <ryanbottriell@live.ca>
2019-08-26 10:31:33 -0700

    Improve coverage and documentation for storage/runtime

--- crates/spfs/spenv/storage/_runtime.py
+++ crates/spfs/spenv/storage/_runtime.py
@@ -12,59 +12,80 @@ import contextlib
 from ._package import Package
 
 
-class MountConfig(NamedTuple):
+class RuntimeConfig(NamedTuple):
+    """Stores the configuration of a single runtime."""
 
     lowerdirs: Tuple[str, ...]
 
+    def dump(self, stream: IO[str]):
+        """Dump this config as json to the given stream."""
+        json.dump(self._asdict(), stream)
+
     @staticmethod
-    def load(stream: IO[str]) -> "MountConfig":
+    def load(stream: IO[str]) -> "RuntimeConfig":
+        """Load a runtime config from the given json stream."""
 
         json_data = json.load(stream)
         json_data["lowerdirs"] = tuple(json_data.get("lowerdirs", []))
-        return MountConfig(**json_data)
-
-    def dump(self, stream: IO[str]):
-        json.dump(self._asdict(), stream)
+        return RuntimeConfig(**json_data)
 
 
 class Runtime:
+    """Represents an active spenv session.
 
-    _upper = "upper"
-    _work = "work"
-    _lower = "lower"
-    dirs = (_upper, _work, _lower)
+    The runtime contains the working files for a spenv
+    envrionment, specifically the work and upper directories
+    that are used by the overlay filesystem mount. It also
+    retains a list of packages and platforms that have been
+    install to the runtime, as well as the resulting stack
+    of read-only filesystem layers.
+    """
 
-    def __init__(self, root: str):
+    _upperdir = "upper"
+    _workdir = "work"
+    _lowerdir = "lower"
+    _configfile = "config.json"
+    dirs = (_upperdir, _workdir, _lowerdir)
+
+    def __init__(self, root: str) -> None:
+        """Create a runtime to represent the data under 'root'."""
 
         self._root: str = os.path.abspath(root)
-        self._config: Optional[MountConfig] = None
+        self._config: Optional[RuntimeConfig] = None
 
     @property
     def ref(self) -> str:
+        """Return the identifier for this runtime."""
         return os.path.basename(self._root)
 
     @property
     def rootdir(self) -> str:
+        """Return the root directory of this runtime."""
         return self._root
 
     @property
-    def lowerdir(self):
-        return os.path.join(self._root, self._lower)
+    def lowerdir(self) -> str:
+        """Return the overlay fs lowerdir of this runtime."""
+        return os.path.join(self._root, self._lowerdir)
 
     @property
-    def configfile(self):
-        return os.path.join(self._root, "config.json")
+    def configfile(self) -> str:
+        """Return the path to this runtimes config file."""
+        return os.path.join(self._root, self._configfile)
 
     @property
-    def upperdir(self):
-        return os.path.join(self._root, self._upper)
+    def upperdir(self) -> str:
+        """Return the overlay fs upperdir of this runtime."""
+        return os.path.join(self._root, self._upperdir)
 
     @property
-    def workdir(self):
-        return os.path.join(self._root, self._work)
+    def workdir(self) -> str:
+        """Return the overlay fs workdir of this runtime."""
+        return os.path.join(self._root, self._workdir)
 
     @property
-    def config(self):
+    def config(self) -> RuntimeConfig:
+        """Return this runtime's configuration data."""
 
         if self._config is None:
             return self._read_config()
@@ -72,33 +93,43 @@ class Runtime:
 
     @property
     def overlay_args(self) -> str:
+        """Return the overlayfs option string needed to mount this runtime."""
         return f"lowerdir={':'.join(self.config.lowerdirs)},upperdir={self.upperdir},workdir={self.workdir}"
 
     def append_package(self, package: Package) -> None:
+        """Append a package to this runtime's stack.
 
-        self._config = MountConfig(self.config.lowerdirs + (package.diffdir,))
+        This will update the configuration of the runtime,
+        and change the overlayfs options, but not update
+        any currently running environment automatically.
+
+        Args:
+            package (Package): The package to append to the stack
+        """
+
+        self._config = RuntimeConfig(self.config.lowerdirs + (package.diffdir,))
         self._write_config()
 
-    def _write_config(self):
+    def _write_config(self) -> None:
 
         with open(self.configfile, "w+", encoding="utf-8") as f:
             self.config.dump(f)
 
-    def _read_config(self) -> MountConfig:
+    def _read_config(self) -> RuntimeConfig:
 
         try:
             with open(self.configfile, "r", encoding="utf-8") as f:
-                self._config = MountConfig.load(f)
+                self._config = RuntimeConfig.load(f)
         except OSError as e:
             if e.errno == errno.ENOENT:
-                self._config = MountConfig(lowerdirs=(self.lowerdir,))
+                self._config = RuntimeConfig(lowerdirs=(self.lowerdir,))
                 self._write_config()
             else:
                 raise
         return self._config
 
 
-def _ensure_runtime(path: str):
+def _ensure_runtime(path: str) -> Runtime:
 
     os.makedirs(path, exist_ok=True, mode=0o777)
     for subdir in Runtime.dirs:
@@ -107,40 +138,76 @@ def _ensure_runtime(path: str):
 
 
 class RuntimeStorage:
+    """Manages the on-disk storage of many runtimes."""
+
     def __init__(self, root: str) -> None:
+        """Initialize a new storage inside the given root directory."""
 
         self._root = os.path.abspath(root)
 
-    def remove_runtime(self, name: str) -> None:
+    def remove_runtime(self, ref: str) -> None:
+        """Remove a runtime forcefully.
 
-        runtime = self.read_runtime(name)
+        Raises:
+            ValueError: If the runtime does not exist
+        """
+
+        runtime = self.read_runtime(ref)
         # TODO: clobber read-only files
         # ensure unmounted first? by removing upperdir?
         shutil.rmtree(runtime.rootdir)
 
-    def read_runtime(self, name: str) -> Runtime:
+    def read_runtime(self, ref: str) -> Runtime:
+        """Access a runtime in this storage.
 
-        runtime_dir = os.path.join(self._root, name)
+        Args:
+            ref (str): The identifier for the runtime to read.
+
+        Raises:
+            ValueError: If the runtime does not exist.
+
+        Returns:
+            Runtime: The identified runtime.
+        """
+
+        runtime_dir = os.path.join(self._root, ref)
         if not os.path.isdir(runtime_dir):
-            raise ValueError("Unknown runtime: " + name)
+            raise ValueError("Unknown runtime: " + ref)
 
         return Runtime(runtime_dir)
 
-    def create_runtime(self, name: str = None) -> Runtime:
+    def create_runtime(self, ref: str = None) -> Runtime:
+        """Create a new runtime.
 
-        if name is None:
-            name = hashlib.sha256(uuid.uuid1().bytes).hexdigest()
+        Args:
+            ref (Optional[str]): the name of the new runtime,
+                defaults to a new generated id.
 
-        runtime_dir = os.path.join(self._root, name)
+        Raises:
+            ValueError: If a runtime with the given ref already exists
+
+        Returns:
+            Runtime: The newly created runtime
+        """
+
+        if ref is None:
+            ref = hashlib.sha256(uuid.uuid1().bytes).hexdigest()
+
+        runtime_dir = os.path.join(self._root, ref)
         try:
             os.makedirs(runtime_dir)
         except OSError as e:
             if e.errno == errno.EEXIST:
-                raise ValueError("Runtime exists: " + name)
+                raise ValueError("Runtime exists: " + ref)
             raise
         return _ensure_runtime(runtime_dir)
 
     def list_runtimes(self) -> List[Runtime]:
+        """List all stored runtimes.
+
+        Returns:
+            List[Runtime]: The stored runtimes.
+        """
 
         try:
             dirs = os.listdir(self._root)
---
+++ crates/spfs/spenv/storage/_runtime_test.py
@@ -0,0 +1,92 @@
+import os
+
+import pytest
+import py.path
+
+from ._runtime import RuntimeStorage, Runtime, RuntimeConfig, _ensure_runtime
+
+
+def test_config_serialization(tmpdir: py.path.local) -> None:
+
+    tmpfile = tmpdir.join("config.json")
+    expected = RuntimeConfig(lowerdirs=("a", "b", "c"))
+    with open(tmpfile.strpath, "w+") as handle:
+        expected.dump(handle)
+    with open(tmpfile.strpath, "r") as handle:
+        actual = RuntimeConfig.load(handle)
+
+    assert actual == expected
+
+
+def test_runtime_properties(tmpdir: py.path.local):
+
+    runtime = Runtime(tmpdir.strpath)
+    assert tmpdir.bestrelpath(runtime.rootdir) == "."
+    assert os.path.basename(runtime.upperdir) == Runtime._upperdir
+    assert os.path.basename(runtime.workdir) == Runtime._workdir
+    assert os.path.basename(runtime.lowerdir) == Runtime._lowerdir
+    assert os.path.basename(runtime.configfile) == Runtime._configfile
+
+
+def test_runtime_config_notnone(tmpdir: py.path.local) -> None:
+
+    runtime = Runtime(tmpdir.strpath)
+    assert runtime._config is None
+    assert runtime.config is not None
+    assert py.path.local(runtime.configfile).exists()
+
+
+def test_runtime_overlay_args_basic_syntax(tmpdir) -> None:
+
+    runtime = Runtime(tmpdir.strpath)
+    args = runtime.overlay_args
+    parts = args.split(",")
+    for part in parts:
+        _, _ = part.split("=")
+
+
+def test_ensure_runtime(tmpdir):
+
+    runtime = _ensure_runtime(tmpdir.join("root").strpath)
+    assert py.path.local(runtime.rootdir).exists()
+    assert py.path.local(runtime.upperdir).exists()
+
+    _ensure_runtime(runtime.rootdir)
+
+
+def test_storage_create_runtime(tmpdir):
+
+    storage = RuntimeStorage(tmpdir.strpath)
+
+    runtime = storage.create_runtime()
+    assert runtime.ref
+    assert py.path.local(runtime.rootdir).isdir()
+
+    with pytest.raises(ValueError):
+        storage.create_runtime(runtime.ref)
+
+
+def test_storage_remove_runtime(tmpdir):
+
+    storage = RuntimeStorage(tmpdir.strpath)
+
+    with pytest.raises(ValueError):
+        storage.remove_runtime("non-existant")
+
+    runtime = storage.create_runtime()
+    storage.remove_runtime(runtime.ref)
+
+
+def test_storage_list_runtimes(tmpdir):
+
+    storage = RuntimeStorage(tmpdir.join("root").strpath)
+
+    assert storage.list_runtimes() == []
+
+    storage.create_runtime()
+    assert len(storage.list_runtimes()) == 1
+
+    storage.create_runtime()
+    storage.create_runtime()
+    storage.create_runtime()
+    assert len(storage.list_runtimes()) == 4
