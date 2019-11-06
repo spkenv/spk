d2380c4d42994bc0880d905c72da525562a95b1d
Ryan Bottriell <rbottriell@imageworks.com>
2019-11-06 15:49:27 -0800

    Use proper version in setup file
    This will remove issues where pex caches the source

--- crates/spfs/setup.py
+++ crates/spfs/setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup, find_packages
 
+import spenv
+
 setup(
     name="spenv",
+    version=spenv.__version__,
     packages=find_packages(),
-    package_data={'spenv': ["*.sh"]},
+    package_data={"spenv": ["*.sh"]},
     include_package_data=True,
     entry_points={"console_scripts": ["spenv=spenv.cli:main"]},
 )
