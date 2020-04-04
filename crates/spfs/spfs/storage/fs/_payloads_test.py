80c7f0a14496776d1e3f50821951c359d0e12833
Ryan Bottriell <rbottriell@imageworks.com>
2020-04-03 17:59:48 -0700

    Fix error where testdata could not be cleaned up

--- crates/spfs/spfs/storage/fs/_payloads_test.py
+++ crates/spfs/spfs/storage/fs/_payloads_test.py
@@ -7,7 +7,7 @@ def test_makedirs_dont_change_existing(tmpdir: py.path.local) -> None:
 
     chkdir = tmpdir.join("my_dir")
     chkdir.ensure(dir=1)
-    chkdir.chmod(755)
+    chkdir.chmod(0o755)
     original = chkdir.stat().mode
     makedirs_with_perms(chkdir + "/new", perms=0o777)
     assert chkdir.stat().mode == original, "existing dir should not change perms"
