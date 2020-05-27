8bedbd57410f6bb1e8052db4e4f146076f78dbee
Ryan Bottriell <rbottriell@imageworks.com>
2020-05-27 13:34:42 -0700

    Improve version parsing and allow restriction

--- spk/cli/_fmt.py
+++ spk/cli/_fmt.py
@@ -6,7 +6,7 @@ import spk
 def format_ident(pkg: spk.api.Ident) -> str:
 
     out = f"{Style.BRIGHT}{pkg.name}{Style.RESET_ALL}"
-    if pkg.version.source:
+    if pkg.version.parts:
         out += f" / {Fore.LIGHTBLUE_EX}{pkg.version}{Fore.RESET}"
     if pkg.build is not None:
         out += f" / {Style.DIM}{pkg.build}{Style.RESET_ALL}"
--- spk/compat/_version.py
+++ spk/compat/_version.py
@@ -1,16 +1,34 @@
-from typing import Union
+from typing import Union, Tuple, Any
 from dataclasses import dataclass
 
+VERSION_SEP = "."
 
-@dataclass
+
 class Version:
     """Version specifies a package version number."""
 
-    source: str
+    def __init__(self, spec: str) -> None:
+
+        if spec:
+            self.parts = tuple(spec.split(VERSION_SEP))
+        else:
+            self.parts = tuple()
 
     def __str__(self) -> str:
 
-        return str(self.source)
+        return str(VERSION_SEP.join(self.parts))
+
+    __repr__ = __str__
+
+    def __eq__(self, other: Any) -> bool:
+
+        if isinstance(other, Version):
+            return self.parts == other.parts
+        return bool(str(self) == other)
+
+    def copy(self) -> "Version":
+
+        return Version(str(self))
 
     def is_satisfied_by(self, other: Union[str, "Version"]) -> bool:
 
@@ -18,7 +36,18 @@ class Version:
             other = Version(other)
 
         # TODO: resolve better and handle ranges
-        return other.source.startswith(self.source)
+        return self.parts == other.parts[: len(self.parts)]
+
+    def restrict(self, other: "Version") -> None:
+
+        if other.is_satisfied_by(self):
+            return
+
+        if not self.is_satisfied_by(other):
+            raise ValueError(f"Cannot restict: {other} is not a subset of {self}")
+
+        print(self.parts, other.parts)
+        self.parts = other.parts
 
 
 def parse_version(version: str) -> Version:
