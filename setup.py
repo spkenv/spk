e6b91315841f585e45239c1d1b954f2162a3e30c
Ryan Bottriell <rbottriell@imageworks.com>
2020-06-10 14:53:28 -0700

    Update for release

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if pipfile.has_section("packages"):
 setup(
     name="spk",
     description="The 'S' Package System: Convenience, clarity and speed.",
-    version="0.1.0",
+    version="0.2.0",
     packages=find_packages(),
     install_requires=install_requires,
     package_data={"": ["Pipfile"]},
--- spk/__init__.py
+++ spk/__init__.py
@@ -1,6 +1,6 @@
 """SPack - an SpFS Package Manager"""
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from . import api, build, storage, solve
 from ._global import load_spec, save_spec
