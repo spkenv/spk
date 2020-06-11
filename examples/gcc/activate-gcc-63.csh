f5217ea84483f187e58c3ac637932b4e80a5f227
Ryan Bottriell <rbottriell@imageworks.com>
2020-06-11 14:55:23 -0700

    Add stub package for gcc 48/63

---
+++ examples/gcc/activate-gcc-63.csh
@@ -0,0 +1,2 @@
+setenv CC /opt/rh/devtoolset-6/root/usr/bin/gcc
+setenv CXX /opt/rh/devtoolset-6/root/usr/bin/gcc
---
+++ examples/gcc/activate-gcc-63.sh
@@ -0,0 +1,2 @@
+export CC=/opt/rh/devtoolset-6/root/usr/bin/gcc
+export CXX=/opt/rh/devtoolset-6/root/usr/bin/gcc
