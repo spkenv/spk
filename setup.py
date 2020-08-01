81bc47747d569f22957c492b4a97d53416658987
Ryan Bottriell <rbottriell@imageworks.com>
2020-07-31 17:21:09 -0700

    Update for release

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if pipfile.has_section("packages"):
 setup(
     name="spk",
     description="The 'S' Package System: Convenience, clarity and speed.",
-    version="0.10.2",
+    version="0.10.3",
     packages=find_packages(),
     install_requires=install_requires,
     package_data={"": ["Pipfile"]},
--- spk.spec
+++ spk.spec
@@ -1,5 +1,5 @@
 Name: spk
-Version: 0.10.2
+Version: 0.10.3
 Release: 1
 Summary: Package manager for SPFS.
 License: NONE
