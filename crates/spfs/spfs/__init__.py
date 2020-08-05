4bdb00542c3d7451b5c722a55e0f45cca55e3519
Ryan Bottriell <rbottriell@imageworks.com>
2020-08-05 12:12:15 -0700

    Update for release

--- crates/spfs/spfs/__init__.py
+++ crates/spfs/spfs/__init__.py
@@ -1,6 +1,6 @@
 """Filesystem isolation, capture and distribution."""
 
-__version__ = "0.20.5"
+__version__ = "0.20.6"
 
 from . import storage, tracking, runtime, io, graph, encoding
 from ._config import get_config, load_config, Config
