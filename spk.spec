791370fba66a69d16b82c0babb8f869875545842
Ryan Bottriell <rbottriell@imageworks.com>
2020-08-13 11:19:08 -0700

    Update for release

--- spk.spec
+++ spk.spec
@@ -1,5 +1,5 @@
 Name: spk
-Version: 0.12.4
+Version: 0.12.5
 Release: 1
 Summary: Package manager for SPFS.
 License: NONE
--- spk/__init__.py
+++ spk/__init__.py
@@ -1,6 +1,6 @@
 """SPack - an SpFS Package Manager"""
 
-__version__ = "0.12.4"
+__version__ = "0.12.5"
 
 from . import api, storage, solve, build, exec
 from ._global import load_spec, save_spec
