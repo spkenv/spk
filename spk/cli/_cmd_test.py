8f8f59736df63f6002ea31670e7ba84ccb4c629b
Ryan Bottriell <rbottriell@imageworks.com>
2021-01-22 14:20:28 -0800

    Add empty cli entrypoint for testing

---
+++ spk/cli/_cmd_test.py
@@ -0,0 +1,52 @@
+from typing import Any, ValuesView
+import argparse
+import os
+import subprocess
+
+import structlog
+
+import spfs
+import spk
+
+from spk.io import format_decision
+from . import _flags
+
+_LOGGER = structlog.get_logger("cli")
+
+
+def register(
+    sub_parsers: argparse._SubParsersAction, **parser_args: Any
+) -> argparse.ArgumentParser:
+
+    test_cmd = sub_parsers.add_parser("test", help=_test.__doc__, **parser_args)
+    test_cmd.add_argument(
+        "packages",
+        metavar="FILE|PKG[@STAGE] ...",
+        nargs="*",
+        default=[""],
+        help="The package(s) to test",
+    )
+    _flags.add_repo_flags(test_cmd)
+    test_cmd.set_defaults(func=_test)
+    return test_cmd
+
+
+_VALID_STAGES = ("sources", "build", "install")
+
+
+def _test(args: argparse.Namespace) -> None:
+    """Run package tests, building as needed."""
+
+    for package in args.packages:
+        name, *stages = package.split("@", 1)
+        stages = stages or _VALID_STAGES
+
+        spec, filename = _flags.find_package_spec(name)
+
+        for stage in stages:
+            if stage not in _VALID_STAGES:
+                raise ValueError(
+                    f"Untestable stage '{stage}', must be one of {_VALID_STAGES}"
+                )
+
+            _LOGGER.info(f"Testing {filename}@{stage}...")
