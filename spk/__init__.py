63cb62e8557c7705228412d999864512c165cb85
Ryan Bottriell <rbottriell@imageworks.com>
2021-02-05 11:27:21 -0800

    Update for release

--- spk/__init__.py
+++ spk/__init__.py
@@ -1,6 +1,6 @@
 """SPack - an SpFS Package Manager"""
 
-__version__ = "0.20.3"
+__version__ = "0.21.0"
 
 from . import api, storage, solve, build, exec, test
 from ._global import load_spec, save_spec
