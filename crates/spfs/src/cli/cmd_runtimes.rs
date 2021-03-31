f1be5f419fcd6cd2d57bf36a2e2cf12b39ebe497
Ryan Bottriell <rbottriell@imageworks.com>
2021-03-31 15:38:27 -0700

    Update runtimes command to output more information

--- crates/spfs/src/cli/cmd_runtimes.rs
+++ crates/spfs/src/cli/cmd_runtimes.rs
@@ -3,14 +3,31 @@ use structopt::StructOpt;
 use spfs;
 
 #[derive(Debug, StructOpt)]
-pub struct CmdRuntimes {}
+pub struct CmdRuntimes {
+    #[structopt(
+        short = "q",
+        long = "quiet",
+        about = "Only print the name of each runtime, no additional data"
+    )]
+    quiet: bool,
+}
 
 impl CmdRuntimes {
     pub fn run(&mut self, config: &spfs::Config) -> spfs::Result<i32> {
         let runtime_storage = config.get_runtime_storage()?;
         for runtime in runtime_storage.iter_runtimes() {
             let runtime = runtime?;
-            println!("{}", runtime.reference().to_string_lossy());
+            let mut message = runtime.reference().to_string_lossy().to_string();
+            if !self.quiet {
+                message = format!(
+                    "{}\trunning={}\tpid={:?}\teditable={}",
+                    message,
+                    runtime.is_running(),
+                    runtime.get_pid(),
+                    runtime.is_editable()
+                )
+            }
+            println!("{}", message);
         }
         Ok(0)
     }
