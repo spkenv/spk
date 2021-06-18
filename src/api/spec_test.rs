00f7597acc9e957e308548dc831b82e0f1198aaa
Ryan Bottriell <ryan@bottriell.ca>
2021-06-17 22:49:14 -0700

    Add test to ensure spec dir is used for default sources
    Signed-off-by: Ryan Bottriell <ryan@bottriell.ca>

--- src/api/spec_test.rs
+++ src/api/spec_test.rs
@@ -1,6 +1,7 @@
 // Copyright (c) 2021 Sony Pictures Imageworks, et al.
 // SPDX-License-Identifier: Apache-2.0
 // https://github.com/imageworks/spk
+use std::io::Write;
 
 use rstest::rstest;
 
@@ -15,4 +16,22 @@ fn test_empty_spec_is_valid() {
 fn test_explicit_no_sources() {
     let spec: Spec = serde_yaml::from_str("sources: []").unwrap();
     assert!(spec.sources.is_empty());
+}
+
+#[rstest]
+fn test_sources_relative_to_spec_file() {
+    let tmpdir = tempdir::TempDir::new("spk_test").unwrap();
+    let spec_dir = tmpdir.path().join("dir");
+    std::fs::create_dir(&spec_dir).unwrap();
+    let spec_file = spec_dir.join("package.spk.yaml");
+    let mut file = std::fs::File::create(&spec_file).unwrap();
+    file.write_all(b"{}").unwrap();
+    drop(file);
+
+    let spec = super::read_spec_file(&spec_file).unwrap();
+    if let Some(super::SourceSpec::Local(local)) = spec.sources.get(0) {
+        assert_eq!(&local.path, &spec_dir);
+    } else {
+        panic!("expected spec to have one local source spec");
+    }
 }
