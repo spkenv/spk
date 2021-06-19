7beff3587fcc3270332f4140d0be2483aa372730
Ryan Bottriell <ryan@bottriell.ca>
2021-06-18 21:34:02 -0700

    Seprate existing validation logic into distinct funtctions
    Signed-off-by: Ryan Bottriell <ryan@bottriell.ca>

---
+++ src/api/validators.rs
@@ -0,0 +1,58 @@
+// Copyright (c) 2021 Sony Pictures Imageworks, et al.
+// SPDX-License-Identifier: Apache-2.0
+// https://github.com/imageworks/spk
+use spfs::tracking::{Diff, DiffMode};
+use std::path::Path;
+
+#[cfg(test)]
+#[path = "./validators_test.rs"]
+mod validators_test;
+
+/// Validates that something was installed for the package
+pub fn must_install_something<P: AsRef<Path>>(diffs: &Vec<Diff>, prefix: P) -> Option<String> {
+    let changes = diffs
+        .into_iter()
+        .filter(|diff| diff.mode != DiffMode::Unchanged)
+        .count();
+
+    if changes == 0 {
+        Some(format!(
+            "Build process created no files under {:?}",
+            prefix.as_ref(),
+        ))
+    } else {
+        None
+    }
+}
+
+/// Validates that the install process did not change
+/// a file that belonged to a build dependency
+pub fn must_not_alter_existing_files<P: AsRef<Path>>(
+    diffs: &Vec<Diff>,
+    _prefix: P,
+) -> Option<String> {
+    for diff in diffs.into_iter() {
+        if let Some((a, b)) = &diff.entries {
+            if a.is_dir() && b.is_dir() {
+                continue;
+            }
+        }
+        if diff.mode == DiffMode::Added {
+            continue;
+        }
+        if diff.mode == DiffMode::Changed {
+            if let Some((a, b)) = &diff.entries {
+                let mode_change = a.mode ^ b.mode;
+                let nonperm_change = (mode_change | 0o777) ^ 0o77;
+                if mode_change != 0 && nonperm_change == 0 {
+                    continue;
+                }
+            }
+        }
+        return Some(format!(
+            "Existing file was {:?}: {:?}",
+            &diff.mode, &diff.path
+        ));
+    }
+    None
+}
--- src/build/binary_test.rs
+++ src/build/binary_test.rs
@@ -1,23 +1,3 @@
 // Copyright (c) 2021 Sony Pictures Imageworks, et al.
 // SPDX-License-Identifier: Apache-2.0
 // https://github.com/imageworks/spk
-use super::validate_build_changeset;
-
-#[test]
-fn test_validate_build_changeset_nothing() {
-    let res = validate_build_changeset(vec![], "/spfs");
-    assert!(res.is_err())
-}
-
-#[test]
-fn test_validate_build_changeset_modified() {
-    let res = validate_build_changeset(
-        vec![spfs::tracking::Diff {
-            path: "/spfs/file.txt".into(),
-            mode: spfs::tracking::DiffMode::Changed,
-            entries: None,
-        }],
-        "/spfs",
-    );
-    assert!(res.is_err())
-}
