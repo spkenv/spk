694ecaa51da7482819a685d6937885e3a258e107
Ryan Bottriell <ryan@bottriell.ca>
2021-06-19 11:09:36 -0700

    Only override request compat when it's not already set
    Signed-off-by: Ryan Bottriell <ryan@bottriell.ca>

--- spk/cli/_flags.py
+++ spk/cli/_flags.py
@@ -202,7 +202,8 @@ def parse_requests_using_flags(
                 request_data.setdefault("prereleasePolicy", "IncludeAll")
 
             req = spk.api.PkgRequest.from_dict(request_data)
-            req.required_compat = "API"
+            if req.required_compat is None:
+                req.required_compat = "API"
             out.append(req)
 
     return out
