4b7a5c274d9b1f8bf73d6e73c180f5acc20e95d4
J Robert Ray <jrray@imageworks.com>
2021-07-24 16:00:56 -0700

    Skeletal Solver methods
    Signed-off-by: J Robert Ray <jrray@imageworks.com>

--- src/solve/solver.rs
+++ src/solve/solver.rs
@@ -3,6 +3,10 @@
 // https://github.com/imageworks/spk
 use pyo3::prelude::*;
 
+use crate::api::OptionMap;
+
+use super::solution::Solution;
+
 #[pyclass]
 pub struct Solver {}
 
@@ -11,5 +15,28 @@ impl Solver {
     #[new]
     fn new() -> Self {
         Solver {}
+    }
+
+    pub fn reset(&mut self) {
+        todo!()
+    }
+
+    /// If true, only solve pre-built binary packages.
+    ///
+    /// When false, the solver may return packages where the build is not set.
+    /// These packages are known to have a source package available, and the requested
+    /// options are valid for a new build of that source package.
+    /// These packages are not actually built as part of the solver process but their
+    /// build environments are fully resolved and dependencies included
+    pub fn set_binary_only(&mut self, _binary_only: bool) {
+        todo!()
+    }
+
+    pub fn solve(&self) -> Solution {
+        todo!()
+    }
+
+    pub fn update_options(&mut self, _options: OptionMap) {
+        todo!()
     }
 }
