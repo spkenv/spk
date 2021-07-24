3940803a1d975094f5324b5082d5bfc20fff0e0a
J Robert Ray <jrray@imageworks.com>
2021-07-24 13:35:16 -0700

    Skeletal solve.Solver class
    Signed-off-by: J Robert Ray <jrray@imageworks.com>

--- src/solve/python.rs
+++ src/solve/python.rs
@@ -3,6 +3,21 @@
 // https://github.com/imageworks/spk
 use pyo3::prelude::*;
 
-pub fn init_module(_py: &Python, _m: &PyModule) -> PyResult<()> {
+use super::solver::Solver;
+
+fn init_submodule_solver(module: &PyModule) -> PyResult<()> {
+    module.add_class::<Solver>()?;
+    Ok(())
+}
+
+pub fn init_module(py: &Python, m: &PyModule) -> PyResult<()> {
+    {
+        let submod_solver = PyModule::new(*py, "_solver")?;
+        init_submodule_solver(submod_solver)?;
+        m.add_submodule(submod_solver)?;
+    }
+
+    m.add_class::<Solver>()?;
+
     Ok(())
 }
