a0886bac2c12a2ad4e6284094a5c42e98ed880d5
J Robert Ray <jrray@imageworks.com>
2021-09-29 11:38:51 -0700

    type `...` implements inherent method `to_string...`
    `impl Display for Digest` produces the same output.
    
    Signed-off-by: J Robert Ray <jrray@imageworks.com>

--- crates/spfs/src/encoding/hash.rs
+++ crates/spfs/src/encoding/hash.rs
@@ -212,9 +212,6 @@ impl<'a> Digest {
     pub fn as_bytes(&'a self) -> &'a [u8] {
         self.0.as_ref()
     }
-    pub fn to_string(&self) -> String {
-        BASE32.encode(self.as_bytes())
-    }
     pub fn from_bytes(digest_bytes: &[u8]) -> Result<Self> {
         match digest_bytes.try_into() {
             Err(err) => Err(Error::new(format!(
