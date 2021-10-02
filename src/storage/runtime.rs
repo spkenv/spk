f56469900a42483385d1e096118a9ea191466229
Ryan Bottriell <ryan@bottriell.ca>
2021-10-02 14:19:24 -0700

    Implement read spec, fix issue in build listing
    Signed-off-by: Ryan Bottriell <ryan@bottriell.ca>

--- src/storage/runtime.rs
+++ src/storage/runtime.rs
@@ -2,9 +2,10 @@
 // SPDX-License-Identifier: Apache-2.0
 // https://github.com/imageworks/spk
 use std::collections::HashMap;
+use std::path::PathBuf;
 
 use super::Repository;
-use crate::{api, Result};
+use crate::{api, Error, Result};
 
 #[derive(Clone, Debug)]
 pub struct RuntimeRepository {
@@ -58,38 +59,48 @@ impl Repository for RuntimeRepository {
     }
 
     fn list_package_builds(&self, pkg: &api::Ident) -> Result<Vec<api::Ident>> {
-        Ok(
-            get_all_filenames(format!("/spfs/spk/pkg/{}/{}", pkg.name(), pkg.version))?
-                .into_iter()
-                .filter_map(|entry| {
-                    if entry.ends_with("/") {
-                        Some(entry[0..entry.len() - 1].to_string())
-                    } else {
-                        None
-                    }
-                })
-                .filter_map(|candidate| match api::parse_build(&candidate) {
-                    Ok(b) => Some(pkg.with_build(Some(b))),
-                    Err(err) => {
-                        tracing::debug!(
-                            "Skipping invalid build in /spfs/spk: [{}] {:?}",
-                            candidate,
-                            err
-                        );
-                        None
-                    }
-                })
-                .collect(),
-        )
+        let mut base = PathBuf::from("/spfs/spk/pkg");
+        base.push(pkg.name());
+        base.push(pkg.version.to_string());
+        Ok(get_all_filenames(&base)?
+            .into_iter()
+            .filter_map(|entry| {
+                if entry.ends_with("/") {
+                    Some(entry[0..entry.len() - 1].to_string())
+                } else {
+                    None
+                }
+            })
+            .filter(|entry| base.join(entry).join("spec.yaml").exists())
+            .filter_map(|candidate| match api::parse_build(&candidate) {
+                Ok(b) => Some(pkg.with_build(Some(b))),
+                Err(err) => {
+                    tracing::debug!(
+                        "Skipping invalid build in /spfs/spk: [{}] {:?}",
+                        candidate,
+                        err
+                    );
+                    None
+                }
+            })
+            .collect())
     }
 
     fn read_spec(&self, pkg: &api::Ident) -> Result<api::Spec> {
-        // try:
-        //     spec_file = os.path.join("/spfs/spk/pkg", str(pkg), "spec.yaml")
-        //     return api.read_spec_file(spec_file)
-        // except FileNotFoundError:
-        //     raise PackageNotFoundError(pkg)
-        todo!()
+        let mut path = PathBuf::from("/spfs/spk/pkg");
+        path.push(pkg.to_string());
+        path.push("spec.yaml");
+
+        match api::read_spec_file(&path) {
+            Err(Error::IO(err)) => {
+                if err.kind() == std::io::ErrorKind::NotFound {
+                    Err(Error::PackageNotFoundError(pkg.clone()))
+                } else {
+                    Err(err.into())
+                }
+            }
+            err => err,
+        }
     }
 
     fn get_package(&self, pkg: &api::Ident) -> Result<spfs::encoding::Digest> {
