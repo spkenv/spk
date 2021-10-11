cbc72fc4c6068878f81e8a1382db1047f83ab26c
Ryan Bottriell <ryan@bottriell.ca>
2021-10-11 14:01:19 -0700

    Ensure that names actually cannot be lowercase, as documented
    Signed-off-by: Ryan Bottriell <ryan@bottriell.ca>

---
+++ src/api/name_test.rs
@@ -0,0 +1,17 @@
+use rstest::rstest;
+
+#[rstest]
+#[case("lowercase")]
+#[case("with-dashes")]
+#[case("num000")]
+#[case("000-000")]
+#[case("-----")]
+#[should_panic]
+#[case("upperCase")] // no upper case
+#[should_panic]
+#[case("has_dashes")] // no underscores
+#[should_panic]
+#[case("name!!")] // no special characters
+fn test_name_validation(#[case] input: &str) {
+    super::validate_name(input).unwrap();
+}
