c5779226bbb4f9cb765f3e2c1e73e9a23e2a1418
Ryan Bottriell <ryan@bottriell.ca>
2021-10-23 15:40:56 -0700

    Replace use of _modules in spkrs.solve
    Signed-off-by: Ryan Bottriell <ryan@bottriell.ca>

--- src/solve/python.rs
+++ src/solve/python.rs
@@ -17,11 +17,6 @@ use super::solution::{PackageSource, Solution};
 use super::solver::{Solver, SolverFailedError};
 use super::validation::{self, Validators, VarRequirementsValidator};
 
-fn init_submodule_errors(py: &Python, module: &PyModule) -> PyResult<()> {
-    module.add("SolverError", py.get_type::<SolverError>())?;
-    Ok(())
-}
-
 #[pyfunction]
 #[pyo3(name = "BuildPackage")]
 fn build_package(
@@ -63,17 +58,6 @@ fn init_submodule_graph(_py: &Python, module: &PyModule) -> PyResult<()> {
     Ok(())
 }
 
-fn init_submodule_solution(_py: &Python, module: &PyModule) -> PyResult<()> {
-    module.add_class::<Solution>()?;
-    Ok(())
-}
-
-fn init_submodule_solver(py: &Python, module: &PyModule) -> PyResult<()> {
-    module.add("SolverFailedError", py.get_type::<SolverFailedError>())?;
-    module.add_class::<Solver>()?;
-    Ok(())
-}
-
 #[pyfunction]
 fn default_validators() -> Vec<Validators> {
     validation::default_validators().into()
@@ -104,10 +88,7 @@ macro_rules! add_submodule {
 }
 
 pub fn init_module(py: &Python, m: &PyModule) -> PyResult<()> {
-    add_submodule!(m, py, "_errors", init_submodule_errors);
     add_submodule!(m, py, "graph", init_submodule_graph);
-    add_submodule!(m, py, "_solver", init_submodule_solver);
-    add_submodule!(m, py, "_solution", init_submodule_solution);
     add_submodule!(m, py, "validation", init_submodule_validation);
 
     m.add_class::<Graph>()?;
