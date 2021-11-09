bdb19da552aa293a4b8da9ec8fef3954df109356
Ryan Bottriell <ryan@bottriell.ca>
2021-11-08 20:47:57 -0800

    Update component spec with name, req, and embedded fields
    Signed-off-by: Ryan Bottriell <ryan@bottriell.ca>

--- src/api/component_spec.rs
+++ src/api/component_spec.rs
@@ -4,6 +4,8 @@
 use pyo3::prelude::*;
 use serde::{Deserialize, Serialize};
 
+use crate::Result;
+
 #[cfg(test)]
 #[path = "./component_spec_test.rs"]
 mod component_spec_test;
@@ -11,4 +13,45 @@ mod component_spec_test;
 /// Defines a named package component.
 #[pyclass]
 #[derive(Debug, Default, Hash, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub struct ComponentSpec {}
+pub struct ComponentSpec {
+    #[pyo3(get)]
+    #[serde(deserialize_with = "deserialize_component_name")]
+    name: String,
+    #[pyo3(get, set)]
+    #[serde(default)]
+    pub requirements: super::RequirementsList,
+    #[pyo3(get, set)]
+    #[serde(default)]
+    pub embedded: super::EmbeddedPackagesList,
+}
+
+impl ComponentSpec {
+    pub fn new<S: Into<String>>(name: S) -> Result<Self> {
+        let name = name.into();
+        super::validate_name(&name)?;
+        Ok(Self {
+            name,
+            requirements: Default::default(),
+            embedded: Default::default(),
+        })
+    }
+
+    /// The name of this component.
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+}
+
+fn deserialize_component_name<'de, D>(deserializer: D) -> std::result::Result<String, D::Error>
+where
+    D: serde::Deserializer<'de>,
+{
+    let value = String::deserialize(deserializer)?;
+    super::validate_name(&value).map_err(|err| {
+        serde::de::Error::invalid_value(
+            serde::de::Unexpected::Str(&value),
+            &err.to_string().as_str(),
+        )
+    })?;
+    Ok(value)
+}
--- src/api/component_spec_test.rs
+++ src/api/component_spec_test.rs
@@ -1,3 +1,26 @@
 // Copyright (c) 2021 Sony Pictures Imageworks, et al.
 // SPDX-License-Identifier: Apache-2.0
 // https://github.com/imageworks/spk
+use rstest::rstest;
+
+use super::ComponentSpec;
+
+#[rstest]
+#[case("valid")]
+#[should_panic]
+#[case("invalid!")]
+#[should_panic]
+#[case("in_valid")]
+fn test_component_name_validation(#[case] name: &str) {
+    ComponentSpec::new(name).unwrap();
+}
+
+#[rstest]
+#[case("name: valid")]
+#[should_panic]
+#[case("name: invalid!")]
+#[should_panic]
+#[case("name: in_valid")]
+fn test_component_name_validation_deserialize(#[case] yaml: &str) {
+    serde_yaml::from_str::<ComponentSpec>(yaml).unwrap();
+}
