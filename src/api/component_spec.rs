5ad49fa11ee038374caac5986b2bfccef139521e
Ryan Bottriell <ryan@bottriell.ca>
2021-11-09 20:33:38 -0800

    Add file pattern parsing to component spec
    Signed-off-by: Ryan Bottriell <ryan@bottriell.ca>

--- src/api/component_spec.rs
+++ src/api/component_spec.rs
@@ -4,7 +4,7 @@
 use pyo3::prelude::*;
 use serde::{Deserialize, Serialize};
 
-use crate::Result;
+use crate::{Error, Result};
 
 #[cfg(test)]
 #[path = "./component_spec_test.rs"]
@@ -12,11 +12,13 @@ mod component_spec_test;
 
 /// Defines a named package component.
 #[pyclass]
-#[derive(Debug, Default, Hash, Clone, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Debug, Hash, Clone, Eq, PartialEq, Serialize, Deserialize)]
 pub struct ComponentSpec {
     #[pyo3(get)]
     #[serde(deserialize_with = "deserialize_component_name")]
     name: String,
+    #[serde(default)]
+    pub files: FileMatcher,
     #[pyo3(get, set)]
     #[serde(default)]
     pub uses: Vec<String>,
@@ -35,6 +37,7 @@ impl ComponentSpec {
         Ok(Self {
             name,
             uses: Default::default(),
+            files: Default::default(),
             requirements: Default::default(),
             embedded: Default::default(),
         })
@@ -43,6 +46,92 @@ impl ComponentSpec {
     /// The name of this component.
     pub fn name(&self) -> &str {
         &self.name
+    }
+}
+
+/// Holds a set of valid file patterns for identifying
+/// files in the spfs filesystem after a build
+#[derive(Debug, Clone)]
+pub struct FileMatcher {
+    rules: Vec<String>,
+    gitignore: ignore::gitignore::Gitignore,
+}
+
+impl Default for FileMatcher {
+    fn default() -> Self {
+        Self {
+            rules: Vec::new(),
+            gitignore: ignore::gitignore::Gitignore::empty(),
+        }
+    }
+}
+
+impl std::hash::Hash for FileMatcher {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.rules.hash(state);
+    }
+}
+
+impl std::cmp::PartialEq for FileMatcher {
+    fn eq(&self, other: &Self) -> bool {
+        self.rules == other.rules
+    }
+}
+
+impl std::cmp::Eq for FileMatcher {}
+
+impl FileMatcher {
+    /// Construct a matcher from the given match patterns
+    ///
+    /// These patterns are expected to be gitignore format
+    /// where the root of the matching is considered to be /spfs.
+    /// This means that to match /spfs/file.txt non-recursively
+    /// you would use the pattern /file.txt
+    pub fn new<P, I>(rules: P) -> Result<Self>
+    where
+        P: Iterator<Item = I>,
+        I: Into<String>,
+    {
+        let rules: Vec<_> = rules.map(Into::into).collect();
+        let mut builder = ignore::gitignore::GitignoreBuilder::new("/");
+        for rule in rules.iter() {
+            builder.add_line(None, rule).map_err(|err| {
+                Error::String(format!("Invalid file pattern '{}': {:?}", rule, err))
+            })?;
+        }
+        let gitignore = builder
+            .build()
+            .map_err(|err| Error::String(format!("Failed to compile file patterns: {:?}", err)))?;
+        Ok(Self { rules, gitignore })
+    }
+
+    /// The list of file match patterns for this component
+    pub fn rules(&self) -> &Vec<String> {
+        &self.rules
+    }
+
+    /// Reports true if the given path matches a rule in this set
+    pub fn matches<P: AsRef<std::path::Path>>(&self, path: P, is_dir: bool) -> bool {
+        self.gitignore.matched(path, is_dir).is_ignore()
+    }
+}
+
+impl<'de> Deserialize<'de> for FileMatcher {
+    fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let files = Vec::<String>::deserialize(deserializer)?;
+        FileMatcher::new(files.into_iter()).map_err(|err| serde::de::Error::custom(err.to_string()))
+    }
+}
+
+impl Serialize for FileMatcher {
+    fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        self.rules.serialize(serializer)
     }
 }
 
--- src/api/component_spec_test.rs
+++ src/api/component_spec_test.rs
@@ -3,7 +3,7 @@
 // https://github.com/imageworks/spk
 use rstest::rstest;
 
-use super::ComponentSpec;
+use super::{ComponentSpec, FileMatcher};
 
 #[rstest]
 #[case("valid")]
@@ -23,4 +23,29 @@ fn test_component_name_validation(#[case] name: &str) {
 #[case("name: in_valid")]
 fn test_component_name_validation_deserialize(#[case] yaml: &str) {
     serde_yaml::from_str::<ComponentSpec>(yaml).unwrap();
+}
+
+#[rstest]
+#[case("{name: valid, files: ['*.yaml']}")]
+fn test_component_files_yaml_roundtrip(#[case] yaml: &str) {
+    let spec = serde_yaml::from_str::<ComponentSpec>(yaml).unwrap();
+    let inter = serde_yaml::to_string(&spec).unwrap();
+    let spec2 = serde_yaml::from_str::<ComponentSpec>(&inter).unwrap();
+    assert_eq!(spec, spec2, "expected no changes going through yaml");
+}
+
+#[rstest]
+#[case(&[], "/file.txt", false)]
+#[case(&["/file.txt"], "/file.txt", true)]
+#[case(&["*.txt"], "/data/file.txt", true)]
+#[case(&["file.txt/"], "/data/file.txt", false)]
+fn test_file_matcher_matching(
+    #[case] patterns: &[&str],
+    #[case] path: &str,
+    #[case] should_match: bool,
+) {
+    // we're not really testing gitignore here, just that the
+    // semantics of our function works as expected
+    let matcher = FileMatcher::new(patterns.iter().map(|s| s.to_string())).unwrap();
+    assert_eq!(matcher.matches(path, path.ends_with("/")), should_match);
 }
