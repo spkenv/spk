d79d52c610c5d2e2cf33ac4ba38275cc85732e0a
Ryan Bottriell <rbottriell@ilm.com>
2021-12-09 10:52:33 -0800

    Define grpc service for core object database type
    Signed-off-by: Ryan Bottriell <rbottriell@ilm.com>

--- crates/spfs/build.rs
+++ crates/spfs/build.rs
@@ -1,6 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure().compile(
         &[
+            "src/proto/defs/database.proto",
             "src/proto/defs/repository.proto",
             "src/proto/defs/tag.proto",
             "src/proto/defs/types.proto",
