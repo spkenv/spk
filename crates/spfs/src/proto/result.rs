c4832eac1546a425346f7350c09d2cc47b70ba0c
Ryan Bottriell <rbottriell@ilm.com>
2021-12-09 11:03:21 -0800

    Make RPCResult implementations for new database types
    Signed-off-by: Ryan Bottriell <rbottriell@ilm.com>

--- crates/spfs/src/proto/result.rs
+++ crates/spfs/src/proto/result.rs
@@ -2,12 +2,18 @@
 // SPDX-License-Identifier: Apache-2.0
 // https://github.com/imageworks/spk
 
-pub(crate) trait RpcResult {
+pub(crate) trait RpcResult: Sized {
     type Ok;
     type Result;
     fn error(err: crate::Error) -> Self;
     fn ok(data: Self::Ok) -> Self;
     fn to_result(self) -> crate::Result<Self::Ok>;
+    fn from_result<T: Into<Self::Ok>>(result: crate::Result<T>) -> Self {
+        match result {
+            Err(err) => Self::error(err),
+            Ok(ok) => Self::ok(ok.into()),
+        }
+    }
 }
 
 macro_rules! handle_error {
@@ -83,3 +89,29 @@ rpc_result!(
     gen::remove_tag_stream_response::Result
 );
 rpc_result!(gen::RemoveTagResponse, gen::remove_tag_response::Result);
+
+rpc_result!(
+    gen::ReadObjectResponse,
+    gen::read_object_response::Result,
+    gen::Object
+);
+rpc_result!(
+    gen::IterDigestsResponse,
+    gen::iter_digests_response::Result,
+    gen::Digest
+);
+rpc_result!(
+    gen::IterObjectsResponse,
+    gen::iter_objects_response::Result,
+    gen::Object
+);
+rpc_result!(
+    gen::WalkObjectsResponse,
+    gen::walk_objects_response::Result,
+    gen::walk_objects_response::WalkObjectsItem
+);
+rpc_result!(gen::WriteObjectResponse, gen::write_object_response::Result);
+rpc_result!(
+    gen::RemoveObjectResponse,
+    gen::remove_object_response::Result
+);
