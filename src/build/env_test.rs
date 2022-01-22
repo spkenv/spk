3deafa8f1b8d428800080cb559304547a4c91789
Ryan Bottriell <ryan@bottriell.ca>
2022-01-22 11:22:20 -0800

    Remove unused functions from rust code
    Signed-off-by: Ryan Bottriell <ryan@bottriell.ca>

--- src/build/env_test.rs
+++ src/build/env_test.rs
@@ -1,31 +1,3 @@
-use rstest::rstest;
-
-#[rstest]
-#[case("$NOTHING", "$NOTHING")]
-#[case("NOTHING", "NOTHING")]
-#[case("$NOTHING:$SOMETHING", "$NOTHING:something")]
-#[case("$SOMETHING:$NOTHING", "something:$NOTHING")]
-#[case("${SOMETHING}$NOTHING", "something$NOTHING")]
-fn test_expand_defined_args(#[case] value: String, #[case] expected: &str) {
-    let get_var = |s: &str| match s {
-        "SOMETHING" => Some("something".to_string()),
-        _ => None,
-    };
-    assert_eq!(super::expand_defined_vars(value, get_var), expected)
-}
-
-#[rstest]
-#[should_panic]
-#[case("$NOTHING", "")]
-#[case("NOTHING", "NOTHING")]
-#[should_panic]
-#[case("$NOTHING:$SOMETHING", "")]
-#[case("$SOMETHING:other", "something:other")]
-#[case("other${SOMETHING}other", "othersomethingother")]
-fn test_expand_vars(#[case] value: String, #[case] expected: &str) {
-    let get_var = |s: &str| match s {
-        "SOMETHING" => Some("something".to_string()),
-        _ => None,
-    };
-    assert_eq!(super::expand_vars(value, get_var).unwrap(), expected)
-}
+// Copyright (c) 2022 Sony Pictures Imageworks, et al.
+// SPDX-License-Identifier: Apache-2.0
+// https://github.com/imageworks/spk
