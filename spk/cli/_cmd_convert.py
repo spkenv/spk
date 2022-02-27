30ee24039f5339e36ef4b92b0ce9091b2559ff3d
Ryan Bottriell <ryan@bottriell.ca>
2022-02-27 08:46:19 -0800

    Remove SPI-specific spcomp2 conversion code
    Signed-off-by: Ryan Bottriell <ryan@bottriell.ca>

--- spk/cli/_cmd_convert.py
+++ spk/cli/_cmd_convert.py
@@ -25,50 +25,6 @@ def register(
     )
 
     sub_parsers = convert_cmd.add_subparsers(dest="converter")
-    spcomp2_cmd = sub_parsers.add_parser(
-        "spcomp2", help="Convert and convert spComp2s as packages", **parser_args
-    )
-    spcomp2_cmd.add_argument(
-        "--target-repo",
-        "-r",
-        type=str,
-        metavar="NAME",
-        default="origin",
-        help="The repository to publish to. Any configured spfs repository can be named here.",
-    )
-    spcomp2_cmd.add_argument(
-        "--publish",
-        default=None,
-        action="store_true",
-        help="Also publish the packages after convert",
-    )
-    spcomp2_cmd.add_argument(
-        "--no-publish",
-        dest="publish",
-        action="store_false",
-        help="Also publish the packages after convert",
-    )
-    spcomp2_cmd.add_argument(
-        "--force",
-        "-f",
-        action="store_true",
-        default=False,
-        help="Forcefully overwrite any existing publishes",
-    )
-    spcomp2_cmd.add_argument(
-        "--no-deps",
-        dest="deps",
-        action="store_false",
-        default=True,
-        help="Do not follow and convert dependencies of the requested spComp2s",
-    )
-    _flags.add_option_flags(spcomp2_cmd)
-    spcomp2_cmd.add_argument(
-        "packages",
-        nargs="+",
-        metavar="NAME[/VERSION]",
-        help="The spcomp2 packages to import (eg: VnP3,  FileSequence/v7)",
-    )
 
     pip_cmd = sub_parsers.add_parser(
         "pip", help="Convert and import packages using pip", **parser_args
@@ -122,10 +78,7 @@ def _convert(args: argparse.Namespace) -> None:
 
     _flags.ensure_active_runtime(args)
 
-    if args.converter == "spcomp2":
-        _convert_spcomp2s(args)
-
-    elif args.converter == "pip":
+    if args.converter == "pip":
         _convert_pip_packages(args)
 
     elif args.converter is None:
@@ -137,40 +90,6 @@ def _convert(args: argparse.Namespace) -> None:
         raise NotImplementedError(
             f"Internal Error: no logic for converter: {args.converter}"
         )
-
-
-def _convert_spcomp2s(args: argparse.Namespace) -> None:
-
-    options = _flags.get_options_from_flags(args)
-
-    specs = []
-    for name in args.packages:
-        if "/" not in name:
-            name += "/" + "current"
-        name, version = name.split("/")
-        specs.extend(
-            spk.external.import_spcomp2(
-                name, version, options=options, recursive=args.deps
-            )
-        )
-
-    print("\nThe following packages were converted:\n")
-    for spec in specs:
-        print(f"  {spk.io.format_ident(spec.pkg)} ", end="")
-        print(spk.io.format_options(spec.resolve_all_options(spk.api.OptionMap())))
-    print("")
-
-    if args.publish is None:
-        print("These packages are now available in the local repository")
-        args.publish = bool(
-            input("Do you want to also publish these packages? [y/N]: ").lower()
-            in ("y", "yes")
-        )
-
-    if args.publish:
-        publisher = spk.Publisher().with_target(args.target_repo).force(args.force)
-        for spec in specs:
-            publisher.publish(spec.pkg)
 
 
 def _convert_pip_packages(args: argparse.Namespace) -> None:
