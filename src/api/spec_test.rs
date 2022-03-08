b2dae50418dac40dd0f6c275026e0e2b466fe63b
Ryan Bottriell <rbottriell@ilm.com>
2022-03-08 15:05:23 -0800

    Fix test that can fail when test dir has symlink
    Signed-off-by: Ryan Bottriell <rbottriell@ilm.com>

--- src/api/spec_test.rs
+++ src/api/spec_test.rs
@@ -21,7 +21,7 @@ fn test_explicit_no_sources() {
 
 #[rstest]
 fn test_sources_relative_to_spec_file(tmpdir: tempdir::TempDir) {
-    let spec_dir = tmpdir.path().join("dir");
+    let spec_dir = tmpdir.path().canonicalize().unwrap().join("dir");
     std::fs::create_dir(&spec_dir).unwrap();
     let spec_file = spec_dir.join("package.spk.yaml");
     let mut file = std::fs::File::create(&spec_file).unwrap();
@@ -30,7 +30,7 @@ fn test_sources_relative_to_spec_file(tmpdir: tempdir::TempDir) {
 
     let spec = super::read_spec_file(&spec_file).unwrap();
     if let Some(super::SourceSpec::Local(local)) = spec.sources.get(0) {
-        assert_eq!(local.path, spec_dir.join("."));
+        assert_eq!(local.path, spec_dir);
     } else {
         panic!("expected spec to have one local source spec");
     }
