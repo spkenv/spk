268f6cb351abf30814830165f3483bca5cb421b4
Ryan Bottriell <rbottriell@ilm.com>
2022-03-23 19:58:12 -0700

    Organize imports to be more rusty, export useful items at the root
    Signed-off-by: Ryan Bottriell <rbottriell@ilm.com>

--- crates/spfs/src/lib.rs
+++ crates/spfs/src/lib.rs
@@ -9,51 +9,54 @@ pub const VERSION: &str = env!("CARGO_PKG_VERSION");
 #[cfg(test)]
 pub mod fixtures;
 
+mod bootstrap;
+mod clean;
+mod commit;
+pub mod config;
+mod diff;
 pub mod encoding;
 pub mod env;
+mod error;
 pub mod graph;
 pub mod io;
+mod ls_tags;
 pub mod prelude;
 pub mod proto;
+mod prune;
+mod resolve;
 pub mod runtime;
 #[cfg(feature = "server")]
 pub mod server;
+mod status;
 pub mod storage;
+mod sync;
 pub mod tracking;
 
-mod error;
-pub use error::{Error, Result};
+// re-exported to make downstream implementations easier
+pub use async_trait::async_trait;
 
-pub mod config;
 pub use self::config::{
     get_config, load_config, open_repository, Config, RemoteAddress, RemoteConfig,
 };
-mod resolve;
-pub use resolve::{
-    compute_manifest, compute_object_manifest, render, render_into_directory,
-    resolve_stack_to_layers, which, which_spfs,
-};
-mod status;
-pub use status::{
-    active_runtime, compute_runtime_manifest, initialize_runtime, make_active_runtime_editable,
-    reinitialize_runtime, remount_runtime,
-};
-mod bootstrap;
 pub use bootstrap::{
     build_command_for_runtime, build_interactive_shell_cmd, build_shell_initialized_command,
 };
-mod sync;
-pub use sync::{pull_ref, push_ref, sync_object, sync_ref};
-mod commit;
-pub use commit::{commit_layer, commit_platform};
-mod clean;
 pub use clean::{
     clean_untagged_objects, get_all_attached_objects, get_all_unattached_objects,
     get_all_unattached_payloads, purge_objects,
 };
-mod prune;
-pub use prune::{get_prunable_tags, prune_tags, PruneParameters};
-mod diff;
+pub use commit::{commit_layer, commit_platform};
 pub use diff::diff;
-mod ls_tags;
+pub use encoding::Digest;
+pub use error::{Error, Result};
 pub use ls_tags::ls_tags;
+pub use prune::{get_prunable_tags, prune_tags, PruneParameters};
+pub use resolve::{
+    compute_manifest, compute_object_manifest, render, render_into_directory,
+    resolve_stack_to_layers, which, which_spfs,
+};
+pub use status::{
+    active_runtime, compute_runtime_manifest, initialize_runtime, make_active_runtime_editable,
+    reinitialize_runtime, remount_runtime,
+};
+pub use sync::{pull_ref, push_ref, sync_object, sync_ref};
