51df643890d02c2f121883156e1640c8360367eb
Ryan Bottriell <ryan@bottriell.ca>
2022-04-01 20:09:18 -0700

    Convert source builder tests to rust
    Signed-off-by: Ryan Bottriell <ryan@bottriell.ca>

--- src/build/sources_test.rs
+++ src/build/sources_test.rs
@@ -3,8 +3,8 @@
 // https://github.com/imageworks/spk
 use rstest::rstest;
 
-use super::validate_source_changeset;
-use crate::fixtures::*;
+use super::{collect_sources, validate_source_changeset};
+use crate::{api, fixtures::*};
 
 #[rstest]
 fn test_validate_sources_changeset_nothing() {
@@ -37,82 +37,103 @@ fn test_validate_sources_changeset_ok() {
 }
 
 #[rstest]
-fn test_sources_subdir(tmpdir: tempdir::TempDir) {
+fn test_sources_subdir(_tmpdir: tempdir::TempDir) {
     let _guard = crate::HANDLE.enter();
-    let _rt = crate::HANDLE.block_on(spfs_runtime());
-    // tar_file = tmpdir.join("archive.tar.gz").strpath
-    // with tarfile.open(tar_file, "w") as tar:
-    //     tar.add("spk/__init__.py")
+    let rt = crate::HANDLE.block_on(spfs_runtime());
 
-    // tar_source = api.TarSource.from_dict(
-    //     # purposefully add leading slash to make sure it doesn't fail
-    //     {"tar": tar_file, "subdir": "/archive/src"}
-    // )
-    // git_source = api.GitSource.from_dict({"git": os.getcwd(), "subdir": "git_repo"})
-    // source_dir = tmpdir.join("source")
-    // source_dir.join("file.txt").ensure()
-    // source_dir.join(".git/gitfile").ensure()
-    // dir_source = api.LocalSource.from_dict(
-    //     {"path": source_dir.strpath, "subdir": "local"}
-    // )
-    // source_file = tmpdir.join("src", "source_file.txt").ensure()
-    // file_source = api.LocalSource.from_dict(
-    //     {"path": source_file.strpath, "subdir": "local"}
-    // )
+    let tar_file = rt.tmpdir.path().join("archive.tar.gz");
+    let writer = std::fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(&tar_file)
+        .unwrap();
+    let mut builder = tar::Builder::new(writer);
+    builder.append_path("src/lib.rs").unwrap();
+    builder.finish().unwrap();
 
-    // dest_dir = tmpdir.join("dest")
-    // spec = api.Spec()
-    // spec.sources = [git_source, tar_source, file_source, dir_source]
-    // collect_sources(spec, dest_dir.strpath)
-    // assert dest_dir.join("local").isdir()
-    // assert dest_dir.join("git_repo").isdir()
-    // assert dest_dir.join("archive/src").isdir()
-    // assert dest_dir.join("archive/src/spk/__init__.py").isfile()
-    // assert dest_dir.join("git_repo/spk/__init__.py").isfile()
-    // assert not dest_dir.join("local/.git").exists(), "should exclude git repo"
-    // assert dest_dir.join("local/file.txt").isfile()
-    // assert dest_dir.join("local/source_file.txt").isfile()
-    todo!()
+    let tar_source = api::TarSource {
+        tar: tar_file.to_string_lossy().to_string(),
+        // purposefully add leading slash to make sure it doesn't fail
+        subdir: Some("/archive/src".to_string()),
+    };
+    let git_source = api::GitSource {
+        git: std::env::current_dir()
+            .unwrap()
+            .to_string_lossy()
+            .to_string(),
+        subdir: Some("git_repo".to_string()),
+        depth: 1,
+        reference: String::new(),
+    };
+    let source_dir = rt.tmpdir.path().join("source");
+    source_dir.join("file.txt").ensure();
+    source_dir.join(".git/gitfile").ensure();
+    let dir_source = api::LocalSource::new(source_dir).set_subdir("local");
+    let source_file = rt.tmpdir.path().join("src").join("source_file.txt");
+    source_file.ensure();
+    let file_source = api::LocalSource::new(source_file).set_subdir("local");
+
+    let dest_dir = rt.tmpdir.path().join("dest");
+    let spec = api::Spec {
+        sources: vec![
+            api::SourceSpec::Git(git_source),
+            api::SourceSpec::Tar(tar_source),
+            api::SourceSpec::Local(file_source),
+            api::SourceSpec::Local(dir_source),
+        ],
+        ..Default::default()
+    };
+    collect_sources(&spec, &dest_dir).unwrap();
+    assert!(dest_dir.join("local").is_dir());
+    assert!(dest_dir.join("git_repo").is_dir());
+    assert!(dest_dir.join("archive/src").is_dir());
+    assert!(dest_dir.join("archive/src/src/lib.rs").is_file());
+    assert!(dest_dir.join("git_repo/spk/__init__.py").is_file());
+    assert!(
+        !dest_dir.join("local/.git").exists(),
+        "should exclude git repo"
+    );
+    assert!(dest_dir.join("local/file.txt").is_file());
+    assert!(dest_dir.join("local/source_file.txt").is_file());
 }
 
 #[rstest]
-fn test_sources_environment(tmpdir: tempdir::TempDir) {
+fn test_sources_environment(_tmpdir: tempdir::TempDir) {
     let _guard = crate::HANDLE.enter();
-    let _rt = crate::HANDLE.block_on(spfs_runtime());
-    // spec = api.Spec.from_dict({"pkg": "sources-test/0.1.0/src"})
-    // expected = "\n".join(
-    //     [
-    //         "SPK_PKG=sources-test/0.1.0/src",
-    //         "SPK_PKG_NAME=sources-test",
-    //         "SPK_PKG_VERSION=0.1.0",
-    //         "SPK_PKG_BUILD=src",
-    //         "SPK_PKG_VERSION_MAJOR=0",
-    //         "SPK_PKG_VERSION_MINOR=1",
-    //         "SPK_PKG_VERSION_PATCH=0",
-    //         "SPK_PKG_VERSION_BASE=0.1.0",
-    //     ]
-    // )
-    // script_source = api.ScriptSource.from_dict(
-    //     {
-    //         "script": [
-    //             "echo SPK_PKG=${SPK_PKG}",
-    //             "echo SPK_PKG_NAME=${SPK_PKG_NAME}",
-    //             "echo SPK_PKG_VERSION=${SPK_PKG_VERSION}",
-    //             "echo SPK_PKG_BUILD=${SPK_PKG_BUILD}",
-    //             "echo SPK_PKG_VERSION_MAJOR=${SPK_PKG_VERSION_MAJOR}",
-    //             "echo SPK_PKG_VERSION_MINOR=${SPK_PKG_VERSION_MINOR}",
-    //             "echo SPK_PKG_VERSION_PATCH=${SPK_PKG_VERSION_PATCH}",
-    //             "echo SPK_PKG_VERSION_BASE=${SPK_PKG_VERSION_BASE}",
-    //         ]
-    //     }
-    // )
-    // dest_dir = tmpdir.join("dest")
-    // spec.sources = [script_source]
-    // collect_sources(spec, dest_dir.strpath)
+    let rt = crate::HANDLE.block_on(spfs_runtime());
+    let mut spec = crate::spec!({"pkg": "sources-test/0.1.0/src"});
+    let expected = vec![
+        "SPK_PKG=sources-test/0.1.0/src",
+        "SPK_PKG_NAME=sources-test",
+        "SPK_PKG_VERSION=0.1.0",
+        "SPK_PKG_BUILD=src",
+        "SPK_PKG_VERSION_MAJOR=0",
+        "SPK_PKG_VERSION_MINOR=1",
+        "SPK_PKG_VERSION_PATCH=0",
+        "SPK_PKG_VERSION_BASE=0.1.0",
+        "",
+    ]
+    .join("\n");
 
-    // out, _ = capfd.readouterr()
-    // assert (
-    //     out.strip() == expected
-    // ), "should have access to package variables in sources script"
-    todo!()
+    let out_file = rt.tmpdir.path().join("out.log");
+    out_file.ensure();
+    let script_source = api::ScriptSource::new([
+        format!("echo SPK_PKG=${{SPK_PKG}} >> {out_file:?}"),
+        format!("echo SPK_PKG_NAME=${{SPK_PKG_NAME}} >> {out_file:?}"),
+        format!("echo SPK_PKG_VERSION=${{SPK_PKG_VERSION}} >> {out_file:?}"),
+        format!("echo SPK_PKG_BUILD=${{SPK_PKG_BUILD}} >> {out_file:?}"),
+        format!("echo SPK_PKG_VERSION_MAJOR=${{SPK_PKG_VERSION_MAJOR}} >> {out_file:?}"),
+        format!("echo SPK_PKG_VERSION_MINOR=${{SPK_PKG_VERSION_MINOR}} >> {out_file:?}"),
+        format!("echo SPK_PKG_VERSION_PATCH=${{SPK_PKG_VERSION_PATCH}} >> {out_file:?}"),
+        format!("echo SPK_PKG_VERSION_BASE=${{SPK_PKG_VERSION_BASE}} >> {out_file:?}"),
+    ]);
+    let dest_dir = rt.tmpdir.path().join("dest");
+    spec.sources = vec![api::SourceSpec::Script(script_source)];
+    collect_sources(&spec, dest_dir).unwrap();
+
+    let actual = std::fs::read_to_string(out_file).unwrap();
+    assert_eq!(
+        actual, expected,
+        "should have access to package variables in sources script"
+    );
 }
