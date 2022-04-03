f2467670295a60c373ee359f22427c5a828b8f5f
Ryan Bottriell <ryan@bottriell.ca>
2022-04-02 18:08:49 -0700

    Update cargo to build an spk binary
    Signed-off-by: Ryan Bottriell <ryan@bottriell.ca>

--- src/cli/bin.rs
+++ src/cli/bin.rs
@@ -1,8 +1,10 @@
-# Copyright (c) 2021 Sony Pictures Imageworks, et al.
-# SPDX-License-Identifier: Apache-2.0
-# https://github.com/imageworks/spk
+// Copyright (c) 2021 Sony Pictures Imageworks, et al.
+// SPDX-License-Identifier: Apache-2.0
+// https://github.com/imageworks/spk
+//! Main entry points and utilities for command line interface and interaction.
 
-"""Main entry points and utilities for command line interface and interaction."""
-
-from ._run import main, run
-from ._args import parse_args, configure_logging, configure_sentry
+fn main() {
+    // code = run(sys.argv[1:])
+    // sentry_sdk.flush()
+    // sys.exit(code)
+}
