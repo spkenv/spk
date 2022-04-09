fbc8fbac66d8de68ae062494ff723f88d547b347
Ryan Bottriell <ryan@bottriell.ca>
2022-04-09 10:21:18 -0700

    Update CI pipeline to not do python validation
    Signed-off-by: Ryan Bottriell <ryan@bottriell.ca>

--- .github/scripts/unittests.sh
+++ .github/scripts/unittests.sh
@@ -6,7 +6,7 @@
 export LANG=en_US.utf8
 
 cd /source
-yum install make rpm-build python36 tcsh deps/*.rpm -y > /dev/null 2>&1
+yum install make rpm-build tcsh deps/*.rpm -y > /dev/null 2>&1
 
 (curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh /dev/stdin -y) > /dev/null 2>&1
 source /root/.cargo/env
@@ -15,9 +15,5 @@ sed -i 's|$HOME|/root|' /root/.bashrc
 
 yum-builddep -y spk.spec > /dev/null 2>&1
 
-pip3 install pipenv > /dev/null 2>&1
-pipenv sync --dev > /dev/null 2>&1
-
 sed -i "s|github.com|$SPFS_PULL_USERNAME:$SPFS_PULL_PASSWORD@github.com|" /source/Cargo.toml
-make devel > /dev/null 2>&1
 make test
