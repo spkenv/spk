22f904ad64bc9bb8a27e6d1b7533367066ac7f74
J Robert Ray <jrray@imageworks.com>
2022-04-28 21:57:13 -0700

    Refactor `RangeIdent::exact`
    Rename to `equals` to match the new naming of `EqualsVersion`, and then
    add a `double_equals` to have a flavor that uses the new
    `DoubleEqualsVersion`. Move common code into a private `new`.
    
    Signed-off-by: J Robert Ray <jrray@imageworks.com>

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -55,7 +55,7 @@ pub use version::{
     VERSION_SEP,
 };
 pub use version_range::{
-    parse_version_range, CompatRange, DoubleEqualsVersion, DoubleNotEqualsVersion, EqualsVersion,
+    parse_version_range, CompatRange, DoubleEqualsVersion, EqualsVersion,
     GreaterThanOrEqualToRange, GreaterThanRange, LessThanOrEqualToRange, LessThanRange,
     LowestSpecifiedRange, NotEqualsVersion, Ranged, SemverRange, VersionFilter, VersionRange,
     WildcardRange, VERSION_RANGE_SEP,
