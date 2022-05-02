672013bd2fc1ef13c8a477d5023ab90f2f56e481
J Robert Ray <jrray@imageworks.com>
2022-05-02 13:10:25 -0700

    Add test for `DigestSearchCriteria::StartsWith`
    Exercise finding digests with a prefix by building up a temporary repo with
    a given set of digests in it and then verify that `find` will return the
    expected matches. The comparison logic is a bit odd because the real
    digests are longer than strings that have been hard-coded here, as in:
    
       "AAA" -> "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA===="
    
    Signed-off-by: J Robert Ray <jrray@imageworks.com>

--- crates/spfs/src/storage/fs/hash_store_test.rs
+++ crates/spfs/src/storage/fs/hash_store_test.rs
@@ -5,7 +5,7 @@
 use rstest::rstest;
 use tokio_stream::StreamExt;
 
-use crate::fixtures::*;
+use crate::{fixtures::*, graph::DigestSearchCriteria, storage::fs::hash_store::PersistableObject};
 
 #[rstest]
 #[tokio::test]
@@ -18,13 +18,68 @@ async fn test_hash_store_iter_states(tmpdir: tempdir::TempDir) {
     }
 }
 
+/// Produce a `Digest` with the desired string
+macro_rules! digest {
+    ($digest:expr) => {
+        $crate::Digest::from_bytes(
+            &data_encoding::BASE32
+                .decode(format!("{:A<digest_size$}", $digest, digest_size = 56).as_bytes())
+                .expect("decode as base32")[..$crate::encoding::DIGEST_SIZE],
+        )
+        .expect("from_bytes")
+    };
+}
+
 #[rstest]
 #[tokio::test]
 async fn test_hash_store_find_digest(tmpdir: tempdir::TempDir) {
     init_logging();
     let store = super::FSHashStore::open(tmpdir.path()).unwrap();
-    let mut stream = store.iter();
-    while stream.next().await.is_some() {
-        panic!("empty hash store should not yield any digests");
+    let content = ["AAA", "ABC", "ABD", "BBB", "BCD", "CCC", "EEE"];
+    for s in content {
+        store
+            .persist_object_with_digest(PersistableObject::EmptyFile, digest!(s))
+            .await
+            .expect("persist digest file");
+    }
+    /*
+    // Uncomment to examine store contents.
+    let output = std::process::Command::new("/usr/bin/find")
+        .arg(tmpdir.path())
+        .output()
+        .expect("ran");
+    std::io::Write::write_all(&mut std::io::stdout(), &output.stdout).expect("write output");
+    */
+    for starts_with in ["A", "AB", "ABC", "ABE", "BB", "DD"] {
+        let mut matches = Vec::new();
+        let mut stream = store.find(DigestSearchCriteria::StartsWith(starts_with.into()));
+        while let Some(Ok(v)) = stream.next().await {
+            matches.push(v);
+        }
+        let original_matches = matches.clone();
+        for control in content {
+            if !control.starts_with(starts_with) {
+                continue;
+            }
+            // Remove the element(s) in `matches` that should have been
+            // found by this control.
+            let len_before = matches.len();
+            matches.retain(|el| !el.to_string().starts_with(control));
+            // Something should have been removed.
+            assert!(
+                len_before > matches.len(),
+                "Using StartsWith({}), {} was not found in matches: {:?}",
+                starts_with,
+                control,
+                original_matches
+            );
+        }
+        // `matches` should be empty after above loop.
+        assert!(
+            matches.is_empty(),
+            "Using StartsWith({}), got unexpected matches: {:?}",
+            starts_with,
+            matches
+        )
     }
 }
