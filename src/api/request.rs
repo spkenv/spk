8e8c8fc80babbb3c6556ad458f49974955d3e2ee
J Robert Ray <jrray@imageworks.com>
2022-06-05 15:53:30 -0700

    Use nom-supreme's `TreeError`
    This crate provides an alternative for `VerboseError` that doesn't throw
    away external errors. Its output is more dense but has less redundant and
    superfluous output compared to `VerboseError`.
    
    Before:
    
    jrray@STRANDED:~/git/spk$ cargo run -- info 'origin/repotest/1.0+a'
        Blocking waiting for file lock on build directory   Compiling spk v0.31.0 (/home/jrray/git/spk)
        Finished dev [unoptimized + debuginfo] target(s) in 29.08s
         Running `target/debug/spk info origin/repotest/1.0+a`
    ERROR Failed to parse request origin/repotest/1.0+a
    
    Caused by:
        0: at line 1, in MapRes:
        origin/repotest/1.0+a
                        ^
    
        1: at line 1, in CompatRange::new_version_range:
        origin/repotest/1.0+a
                        ^
    
        2: at line 1, in Alt:
        origin/repotest/1.0+a
                        ^
    
        3: at line 1, in Alt:
        origin/repotest/1.0+a
                        ^
    
        4: at line 1, in version_range:
        origin/repotest/1.0+a
                        ^
    
        5: at line 1, in range_ident_version_filter:
        origin/repotest/1.0+a
                        ^
    
        6: at line 1, in parse_version_filter:
        origin/repotest/1.0+a
                        ^
    
        7: at line 1, in Alt:
        origin/repotest/1.0+a
                       ^
    
        8: at line 1, in range ident version range:
        origin/repotest/1.0+a
                       ^
    
    After:
    
    jrray@STRANDED:~/git/spk$ cargo run -- info 'origin/repotest/1.0+a'
        Finished dev [unoptimized + debuginfo] target(s) in 0.11s
         Running `target/debug/spk info origin/repotest/1.0+a`
    ERROR Failed to parse request origin/repotest/1.0+a
    
    Caused by:
        in section "range ident version range" at /1.0+a,
        one of:
          expected eof at /1.0+a, or
          in section "parse_version_filter" at 1.0+a,
          in section "range_ident_version_filter" at 1.0+a,
          in section "version_range" at 1.0+a,
          one of:
            expected '^' at 1.0+a, or
            expected '~' at 1.0+a, or
            error in Tag at 1.0+a, or
            error in Tag at 1.0+a, or
            expected '>' at 1.0+a, or
            expected '<' at 1.0+a, or
            error in Tag at 1.0+a, or
            expected '=' at 1.0+a, or
            error in Tag at 1.0+a, or
            error in Tag at 1.0+a, or
            external error:
              Expected exactly one wildcard in version range, got: 1.0+a at 1.0+a, or
            in section "CompatRange::new_version_range" at 1.0+a,
            external error:
              InvalidVersionError(InvalidVersionError { message: "Version tag segment must be of the form <name>.<int>, got [a]" }) at 1.0+a
    
    Signed-off-by: J Robert Ray <jrray@imageworks.com>

--- src/api/request.rs
+++ src/api/request.rs
@@ -305,12 +305,10 @@ impl FromStr for RangeIdent {
     type Err = crate::Error;
 
     fn from_str(s: &str) -> crate::Result<Self> {
-        crate::parsing::range_ident::<nom::error::VerboseError<_>>(&KNOWN_REPOSITORY_NAMES, s)
+        crate::parsing::range_ident::<nom_supreme::error::ErrorTree<_>>(&KNOWN_REPOSITORY_NAMES, s)
             .map(|(_, ident)| ident)
             .map_err(|err| match err {
-                nom::Err::Error(e) | nom::Err::Failure(e) => {
-                    crate::Error::String(nom::error::convert_error(s, e))
-                }
+                nom::Err::Error(e) | nom::Err::Failure(e) => crate::Error::String(e.to_string()),
                 nom::Err::Incomplete(_) => unreachable!(),
             })
     }
