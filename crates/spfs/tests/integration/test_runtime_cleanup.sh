2d7c89a9e24336e4d786b43e1fcb95476e7b43c7
J Robert Ray <jrobertray@gmail.com>
2022-06-25 16:27:08 -0700

    Fix false negative in runtime cleanup test
    The previous integration test creates a runtime and not enough time passes
    for it to disappear before this test expects only one to exist.
    
    Signed-off-by: J Robert Ray <jrobertray@gmail.com>

--- crates/spfs/tests/integration/test_runtime_cleanup.sh
+++ crates/spfs/tests/integration/test_runtime_cleanup.sh
@@ -9,11 +9,17 @@ set -o errexit
 # some simple tests to ensure that runtimes are properly cleaned up
 
 assert_runtime_count() {
+    # give any runtimes created by other tests a chance to expire
+    # this number relates to the 2.5s poll interval when not using
+    # cnproc.
+    sleep 3
+
     count=$(spfs runtime list -q | wc -l)
     test $count -eq $1
 }
 
 # there's a runtime inside but not once exited
+assert_runtime_count 0
 inner_count=$(spfs run - -- spfs runtime list -q | wc -l)
 test $inner_count -eq 1
 assert_runtime_count 0
@@ -23,21 +29,16 @@ spfs run - -- sleep 4 &
 spfs run - -- sleep 4 &
 spfs run - -- sleep 4 &
 spfs run - -- sleep 4 &
-sleep 2
 assert_runtime_count 4
 wait
-sleep 2
 assert_runtime_count 0
 
 # many runtimes launched recursively
 spfs run - -- spfs run - -- spfs run - -- spfs run - -- sleep 4 &
-sleep 2
 assert_runtime_count 4
 wait
-sleep 2
 assert_runtime_count 0
 
 # fast runtime doesn't linger
 spfs run - :
-sleep 2
 assert_runtime_count 0
