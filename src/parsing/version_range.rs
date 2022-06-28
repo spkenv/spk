e7be3367e3ec9df9930bd3fc82e28c335a2af5c9
J Robert Ray <jrray@imageworks.com>
2022-06-28 00:59:35 -0700

    Rework `CompatRange` and `WildcardRange` parsing
    Parsing version ranges that had versions with multiple tag sets was not
    working, such as:
    
        pkg/1.0+a.0,A.0,2.0+b.0,B.0
    
    Because of the ambiguity with numeric-only tag names, the property testing
    was only generating inputs with a single tag in the tag set. But if changed
    to generate non-ambiguous cases, it reveals that these types of values
    aren't parsed correctly.
    
    Don't assume everything up to the first ',' is the complete version range.
    
    Switch parsing `CompatRange` to nom parsing and require a compatibility
    prefix to detect this case, before trying other alternate forms. Change
    `wildcard_range` to require a wildcard, so this alternate only matches true
    wildcard expressions. Handling empty inputs has moved elsewhere. Happily
    this function no longer takes boolean arguments.
    
    Add an alternate that handles the plain version number expression. This is
    last so it doesn't consume input like `0.0.*.0.0`. This should be consumed
    by `wildcard_range` but would be partially consumed by `compat_range` if
    that allowed an optional compatibility prefix.
    
    This is a backport from #370 with some additional changes to make the tests
    succeed without the change to require tag names have some non-numeric
    character. It now succeeds at parsing but in the situation where an input
    has two legal interpretations, it will always prefer to interpret extra
    tag set entries rather than a separate range filter.
    
    Signed-off-by: J Robert Ray <jrray@imageworks.com>

--- src/parsing/version_range.rs
+++ src/parsing/version_range.rs
@@ -7,47 +7,32 @@ use std::convert::TryFrom;
 use nom::{
     branch::alt,
     character::complete::{char, digit1},
-    combinator::{all_consuming, cut, map, map_res, rest},
+    combinator::{cut, map, map_res, verify},
     error::{context, ContextError, FromExternalError, ParseError},
-    multi::{separated_list0, separated_list1},
-    sequence::preceded,
-    FindToken, IResult,
+    multi::separated_list1,
+    sequence::{pair, preceded, terminated},
+    IResult,
 };
 use nom_supreme::tag::{complete::tag, TagError};
 
 use crate::api::{
-    CompatRange, DoubleEqualsVersion, DoubleNotEqualsVersion, EqualsVersion,
+    CompatRange, CompatRule, DoubleEqualsVersion, DoubleNotEqualsVersion, EqualsVersion,
     GreaterThanOrEqualToRange, GreaterThanRange, LessThanOrEqualToRange, LessThanRange,
     LowestSpecifiedRange, NotEqualsVersion, SemverRange, VersionFilter, VersionRange,
     WildcardRange,
 };
 
-use super::{
-    parse_until,
-    version::{version, version_str},
-};
+use super::version::{version, version_str};
 
-/// Parse a wildcard range into a [`VersionRange`].
+/// Parse a compat range into a [`VersionRange`].
 ///
-/// If `require_star` is true, the input is only considered valid
-/// if it contains a `*` character. Otherwise, an empty string
-/// is a valid input and is interpreted as if the input was `"*"`.
-///
-/// If `fail_if_contains_star` is true, the parser will terminate
-/// with an "unrecoverable" failure instead of a normal error if
-/// the input contains a `*` character.
-///
-/// At most one wildcard is allowed.
+/// A compat range is a plain version number preceded by a compatibility
+/// requirement.
 ///
 /// Examples:
-/// - `""`
-/// - `"*"`
-/// - `"1.*"`
-/// - `"*.1"`
-pub(crate) fn wildcard_range<'a, E>(
-    require_star: bool,
-    fail_if_contains_star: bool,
-) -> impl Fn(&'a str) -> IResult<&'a str, VersionRange, E>
+/// - Binary:1.0
+/// - API:1.0
+pub(crate) fn compat_range<'a, E>(input: &'a str) -> IResult<&'a str, VersionRange, E>
 where
     E: ParseError<&'a str>
         + ContextError<&'a str>
@@ -55,39 +40,64 @@ where
         + FromExternalError<&'a str, std::num::ParseIntError>
         + TagError<&'a str, &'static str>,
 {
-    move |input| {
-        let mut parser = all_consuming(map_res(
-            separated_list0(
+    map(
+        pair(terminated(compat_rule, char(':')), cut(version)),
+        |(required, base)| {
+            VersionRange::Compat(CompatRange {
+                base,
+                required: Some(required),
+            })
+        },
+    )(input)
+}
+
+/// Parse a [`CompatRule'].
+///
+/// This is either the word "Binary" or "API".
+pub(crate) fn compat_rule<'a, E>(input: &'a str) -> IResult<&'a str, CompatRule, E>
+where
+    E: ParseError<&'a str> + TagError<&'a str, &'static str>,
+{
+    alt((
+        map(tag("Binary"), |_| CompatRule::Binary),
+        map(tag("API"), |_| CompatRule::API),
+    ))(input)
+}
+
+/// Parse a wildcard range into a [`VersionRange`].
+///
+/// One wildcard is required.
+///
+/// Examples:
+/// - `"*"`
+/// - `"1.*"`
+/// - `"*.1"`
+pub(crate) fn wildcard_range<'a, E>(input: &'a str) -> IResult<&'a str, VersionRange, E>
+where
+    E: ParseError<&'a str>
+        + ContextError<&'a str>
+        + FromExternalError<&'a str, crate::error::Error>
+        + FromExternalError<&'a str, std::num::ParseIntError>
+        + TagError<&'a str, &'static str>,
+{
+    map(
+        verify(
+            separated_list1(
                 tag("."),
                 alt((
                     map_res(digit1, |n: &str| n.parse::<u32>().map(Some)),
                     map(tag("*"), |_| None),
                 )),
             ),
-            |parts| {
-                if parts.is_empty() && !require_star {
-                    WildcardRange::new_version_range("*")
-                } else if parts.iter().filter(|p| p.is_none()).count() != 1 {
-                    Err(crate::Error::String(format!(
-                        "Expected exactly one wildcard in version range, got: {input}"
-                    )))
-                } else {
-                    Ok(VersionRange::Wildcard(WildcardRange {
-                        specified: parts.len(),
-                        parts,
-                    }))
-                }
-            },
-        ));
-        if fail_if_contains_star && input.find_token('*') {
-            // This `cut` is so if the input contains '*' but parsing
-            // fails, this becomes a hard error instead of trying other
-            // alternates (in the outer scope).
-            cut(parser)(input)
-        } else {
-            parser(input)
-        }
-    }
+            |parts: &Vec<Option<u32>>| parts.iter().filter(|p| p.is_none()).count() == 1,
+        ),
+        |parts| {
+            VersionRange::Wildcard(WildcardRange {
+                specified: parts.len(),
+                parts,
+            })
+        },
+    )(input)
 }
 
 /// Parse a version filter into a [`VersionRange`].
@@ -109,13 +119,7 @@ where
 /// - `"^1.0"`
 /// - `"~1.0"`
 /// - `">1.0,<2.0"`
-///
-/// See [wildcard_range] for an explanation of `require_star` and
-/// `fail_if_contains_star`.
-pub(crate) fn version_range<'a, E>(
-    require_star: bool,
-    fail_if_contains_star: bool,
-) -> impl Fn(&'a str) -> IResult<&'a str, VersionRange, E>
+pub(crate) fn version_range<'a, E>(input: &'a str) -> IResult<&'a str, VersionRange, E>
 where
     E: ParseError<&'a str>
         + ContextError<&'a str>
@@ -123,75 +127,72 @@ where
         + FromExternalError<&'a str, std::num::ParseIntError>
         + TagError<&'a str, &'static str>,
 {
-    move |input: &str| {
-        context(
-            "version_range",
-            map(
-                separated_list1(
-                    tag(crate::api::VERSION_RANGE_SEP),
-                    parse_until(
-                        crate::api::VERSION_RANGE_SEP,
-                        alt((
-                            // Use `cut` for these that first match on an operator first,
-                            // if the version fails to parse then it shouldn't continue to
-                            // try the other options of the `alt` here.
-                            map_res(
-                                preceded(char('^'), cut(version_str)),
-                                SemverRange::new_version_range,
-                            ),
-                            map_res(preceded(char('~'), cut(version)), |v| {
-                                LowestSpecifiedRange::try_from(v).map(VersionRange::LowestSpecified)
-                            }),
-                            map_res(
-                                preceded(tag(">="), cut(version_str)),
-                                GreaterThanOrEqualToRange::new_version_range,
-                            ),
-                            map_res(
-                                preceded(tag("<="), cut(version_str)),
-                                LessThanOrEqualToRange::new_version_range,
-                            ),
-                            map_res(
-                                preceded(char('>'), cut(version_str)),
-                                GreaterThanRange::new_version_range,
-                            ),
-                            map_res(
-                                preceded(char('<'), cut(version_str)),
-                                LessThanRange::new_version_range,
-                            ),
-                            map(
-                                preceded(tag("=="), cut(version)),
-                                DoubleEqualsVersion::version_range,
-                            ),
-                            map(
-                                preceded(char('='), cut(version)),
-                                EqualsVersion::version_range,
-                            ),
-                            map(preceded(tag("!=="), cut(version)), |v| {
-                                VersionRange::DoubleNotEquals(DoubleNotEqualsVersion::from(v))
-                            }),
-                            map(preceded(tag("!="), cut(version)), |v| {
-                                VersionRange::NotEquals(NotEqualsVersion::from(v))
-                            }),
-                            alt((
-                                wildcard_range(require_star, fail_if_contains_star),
-                                context(
-                                    "CompatRange::new_version_range",
-                                    map_res(rest, CompatRange::new_version_range),
-                                ),
-                            )),
-                        )),
+    context(
+        "version_range",
+        map(
+            separated_list1(
+                tag(crate::api::VERSION_RANGE_SEP),
+                alt((
+                    // Use `cut` for these that first match on an operator first,
+                    // if the version fails to parse then it shouldn't continue to
+                    // try the other options of the `alt` here.
+                    map_res(
+                        preceded(char('^'), cut(version_str)),
+                        SemverRange::new_version_range,
                     ),
-                ),
-                |mut version_range| {
-                    if version_range.len() == 1 {
-                        version_range.remove(0)
-                    } else {
-                        VersionRange::Filter(VersionFilter {
-                            rules: version_range.into_iter().collect(),
+                    map_res(preceded(char('~'), cut(version)), |v| {
+                        LowestSpecifiedRange::try_from(v).map(VersionRange::LowestSpecified)
+                    }),
+                    map_res(
+                        preceded(tag(">="), cut(version_str)),
+                        GreaterThanOrEqualToRange::new_version_range,
+                    ),
+                    map_res(
+                        preceded(tag("<="), cut(version_str)),
+                        LessThanOrEqualToRange::new_version_range,
+                    ),
+                    map_res(
+                        preceded(char('>'), cut(version_str)),
+                        GreaterThanRange::new_version_range,
+                    ),
+                    map_res(
+                        preceded(char('<'), cut(version_str)),
+                        LessThanRange::new_version_range,
+                    ),
+                    map(
+                        preceded(tag("=="), cut(version)),
+                        DoubleEqualsVersion::version_range,
+                    ),
+                    map(
+                        preceded(char('='), cut(version)),
+                        EqualsVersion::version_range,
+                    ),
+                    map(preceded(tag("!=="), cut(version)), |v| {
+                        VersionRange::DoubleNotEquals(DoubleNotEqualsVersion::from(v))
+                    }),
+                    map(preceded(tag("!="), cut(version)), |v| {
+                        VersionRange::NotEquals(NotEqualsVersion::from(v))
+                    }),
+                    compat_range,
+                    wildcard_range,
+                    // Just a plain version can be a version range.
+                    map(version, |base| {
+                        VersionRange::Compat(CompatRange {
+                            base,
+                            required: None,
                         })
-                    }
-                },
+                    }),
+                )),
             ),
-        )(input)
-    }
+            |mut version_range| {
+                if version_range.len() == 1 {
+                    version_range.remove(0)
+                } else {
+                    VersionRange::Filter(VersionFilter {
+                        rules: version_range.into_iter().collect(),
+                    })
+                }
+            },
+        ),
+    )(input)
 }
