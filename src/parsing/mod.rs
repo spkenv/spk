55ee1a4df8b27b8f4050569327b71089a1153208
J Robert Ray <jrray@jrray.org>
2022-07-12 11:59:09 -0700

    Improve error message when parsing versions
        ERROR Failed to parse request mypkg/=1.*
    
        Caused by:
            Error: expected an ascii digit at *
    
    Previously:
    
        Caused by:
            Error: expected eof at .*
    
    Using the same approach as `cut` on parsing the 2nd or later digits means
    that unexpected input can be properly attributed to the version number
    being malformed, rather than successfully parsing the version as "1" and
    then having unexpected leftover input.
    
    Signed-off-by: J Robert Ray <jrray@jrray.org>

--- src/parsing/mod.rs
+++ src/parsing/mod.rs
@@ -19,7 +19,7 @@ use nom::{
     combinator::{cut, eof, fail, opt, peek},
     error::{ContextError, FromExternalError, ParseError},
     sequence::{pair, preceded, terminated},
-    IResult, Parser,
+    IResult, InputLength, Parser,
 };
 
 pub(crate) use ident::ident;
@@ -134,6 +134,58 @@ where
             )),
         ),
     ))
+}
+
+/// A clone of `separated_list1` but uses `cut` on the 2nd and later elements.
+pub(crate) fn separated_list1_with_cut<I, O, O2, E, F, G>(
+    mut sep: G,
+    mut f: F,
+) -> impl FnMut(I) -> IResult<I, Vec<O>, E>
+where
+    I: Clone + InputLength,
+    F: Parser<I, O, E>,
+    G: Parser<I, O2, E>,
+    E: ParseError<I>,
+{
+    use nom::{error::ErrorKind, Err};
+
+    move |mut i: I| {
+        let mut res = Vec::new();
+
+        // Parse the first element
+        match f.parse(i.clone()) {
+            Err(e) => return Err(e),
+            Ok((i1, o)) => {
+                res.push(o);
+                i = i1;
+            }
+        }
+
+        loop {
+            let len = i.input_len();
+            match sep.parse(i.clone()) {
+                Err(Err::Error(_)) => return Ok((i, res)),
+                Err(e) => return Err(e),
+                Ok((i1, _)) => {
+                    // infinite loop check: the parser must always consume
+                    if i1.input_len() == len {
+                        return Err(Err::Error(E::from_error_kind(i1, ErrorKind::SeparatedList)));
+                    }
+
+                    match f.parse(i1.clone()) {
+                        // Deviates from `separated_list1` here; an error is
+                        // a failure.
+                        Err(Err::Error(e)) => return Err(Err::Failure(e)),
+                        Err(e) => return Err(e),
+                        Ok((i2, o)) => {
+                            res.push(o);
+                            i = i2;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
 /// Expect a version-like expression and optional build.
