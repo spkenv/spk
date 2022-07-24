b8aba9e6f6e28d4d355220a9e9729bf375a24afb
J Robert Ray <jrray@jrray.org>
2022-07-24 13:31:39 -0700

    Add test/note about nested embedded packages
    Signed-off-by: J Robert Ray <jrray@jrray.org>

--- crates/spk-schema/src/embedded_packages_list_test.rs
+++ crates/spk-schema/src/embedded_packages_list_test.rs
@@ -23,3 +23,24 @@ fn test_install_embedded_build_options() {
     )
     .is_err());
 }
+
+#[rstest]
+fn test_embedded_nested_embedded() {
+    // XXX Demonstrate that within an embedded package list, an embedded
+    // package is able to define a component that embeds a nested package,
+    // allowing for unbounded nesting of embedded packages.
+    //
+    // The embed stub code does not support creating stubs for nested embeds.
+    // Decide if there is a use case for this and start supporting it, or
+    // stop allowing this to be a valid package.
+    let _spec: EmbeddedPackagesList = serde_yaml::from_str(
+        r#"
+          - pkg: "embedded/1.0.0"
+            components:
+              - name: run
+                embedded:
+                  - pkg: "nested-embedded/2.0.0"
+        "#,
+    )
+    .unwrap();
+}
