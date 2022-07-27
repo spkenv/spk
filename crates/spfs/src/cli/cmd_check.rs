67c8fb3544655125d1a197ee6f703445129c6790
J Robert Ray <jrray@jrray.org>
2022-07-26 21:44:22 -0700

    Add a `--pull` flag to `spfs check`
    This will be used to enable some kinds of repairs.
    
    Signed-off-by: J Robert Ray <jrray@jrray.org>

--- crates/spfs/src/cli/cmd_check.rs
+++ crates/spfs/src/cli/cmd_check.rs
@@ -12,11 +12,38 @@ pub struct CmdCheck {
     /// Trigger the check operation on a remote repository instead of the local one
     #[clap(short, long)]
     remote: Option<String>,
+
+    /// Attempt to fix problems by pulling from another repository. Defaults to "origin".
+    #[clap(long)]
+    pull: Option<Option<String>>,
 }
 
 impl CmdCheck {
     pub async fn run(&mut self, config: &spfs::Config) -> spfs::Result<i32> {
         let repo = spfs::config::open_repository_from_string(config, self.remote.as_ref()).await?;
+
+        let _pull_from = match self.pull.take() {
+            Some(name @ Some(_)) if name == self.remote => {
+                return Err("Cannot --pull from same repo as --remote".into());
+            }
+            Some(None)
+                if self
+                    .remote
+                    .as_ref()
+                    .map(|r| r == "origin")
+                    .unwrap_or_default() =>
+            {
+                return Err("Cannot --pull from same repo as --remote".into());
+            }
+            Some(mut repo) => Some(
+                spfs::config::open_repository_from_string(
+                    config,
+                    repo.take().or_else(|| Some("origin".to_owned())),
+                )
+                .await?,
+            ),
+            None => None,
+        };
 
         tracing::info!("walking repository...");
         let errors = match repo {
