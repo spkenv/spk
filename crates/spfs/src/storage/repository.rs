52a6c4d5660f67b3f8eaceecfc46857d16c36377
J Robert Ray <jrray@jrray.org>
2022-08-16 22:49:54 -0700

    Add `Repository::renders_for_all_users`
    Like `Repository::renders` but this will inspect the contents of the repo
    and return an opened repository for every user [subdirectory] found inside
    the renders directory.
    
    Signed-off-by: J Robert Ray <jrray@jrray.org>

--- crates/spfs/src/storage/repository.rs
+++ crates/spfs/src/storage/repository.rs
@@ -60,6 +60,15 @@ pub trait Repository:
         Err(Error::NoRenderStorage(self.address()))
     }
 
+    /// If supported, returns a list of the render storage for all the users
+    /// with renders found in the repository.
+    ///
+    /// # Errors:
+    /// - [`Error::NoRenderStorage`] - if this repository does not support manifest rendering
+    fn renders_for_all_users(&self) -> Result<Vec<Box<dyn ManifestViewer>>> {
+        Err(Error::NoRenderStorage(self.address()))
+    }
+
     /// Return true if this repository contains the given reference.
     async fn has_ref(&self, reference: &str) -> bool {
         self.read_ref(reference).await.is_ok()
