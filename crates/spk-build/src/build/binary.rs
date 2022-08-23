3456bac1c2a497cfcbdff4eb5599ab62fbb16975
J Robert Ray <jrray@jrray.org>
2022-08-22 23:19:07 -0700

    Fix builds (sometimes) not using bash
    Fixes #464. Using setenv in a multi-threaded program is discouraged. I
    wasn't able to reproduce the problem in a trivial package but in a complex
    package I was able to get the spk build script `echo $SHELL` to print tcsh,
    even though it was in fact running the build script with bash. However,
    the spfs runtime environment was created with tcsh.
    
    Signed-off-by: J Robert Ray <jrray@jrray.org>

--- crates/spk-build/src/build/binary.rs
+++ crates/spk-build/src/build/binary.rs
@@ -454,11 +454,6 @@ where
             BuildSource::LocalPath(path) => path.clone(),
         };
 
-        // force the base environment to be setup using bash, so that the
-        // spfs startup and build environment are predictable and consistent
-        // (eg in case the user's shell does not have startup scripts in
-        //  the dependencies, is not supported by spfs, etc)
-        std::env::set_var("SHELL", "bash");
         let runtime = spfs::active_runtime().await?;
         let cmd = if self.interactive {
             println!("\nNow entering an interactive build shell");
@@ -485,6 +480,11 @@ where
         cmd.envs(options.to_environment());
         cmd.envs(get_package_build_env(package));
         cmd.env("PREFIX", &self.prefix);
+        // force the base environment to be setup using bash, so that the
+        // spfs startup and build environment are predictable and consistent
+        // (eg in case the user's shell does not have startup scripts in
+        //  the dependencies, is not supported by spfs, etc)
+        cmd.env("SHELL", "bash");
         cmd.current_dir(&source_dir);
 
         match cmd
