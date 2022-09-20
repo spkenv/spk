4681b9219dc46cdf44d2fb9041f3cd7430add7f0
Ryan Bottriell <ryan@bottriell.ca>
2022-09-20 08:11:43 -0700

    Add helper for setting default tpl variable values
    Signed-off-by: Ryan Bottriell <ryan@bottriell.ca>

--- crates/spk-schema/src/spec_test.rs
+++ crates/spk-schema/src/spec_test.rs
@@ -2,7 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0
 // https://github.com/imageworks/spk
 use rstest::rstest;
-use spk_schema_foundation::{name::PkgName, option_map, spec_ops::Versioned};
+use spk_schema_foundation::{
+    name::PkgName,
+    option_map,
+    spec_ops::{Named, Versioned},
+};
 
 use crate::Template;
 
@@ -50,4 +54,32 @@ sources:
         .render(&options)
         .expect("template should not fail to render");
     assert_eq!(recipe.version().to_string(), "2.3.4");
+}
+
+#[rstest]
+fn test_template_rendering_default_value() {
+    // because we require a value for all variables in the template
+    // a helper must be provided that allows for defining default values
+    // as a convenience
+
+    static SPEC: &str = r#"
+{{ default name "my-package" }}
+pkg: {{ name }}/{{ version }}
+sources:
+  - git: https://downloads.testing/{{ name }}/v{{ version }}
+"#;
+    let tpl = SpecTemplate {
+        name: PkgName::new("my-package").unwrap().to_owned(),
+        file_path: "my-package.spk.yaml".into(),
+        template: SPEC.to_string(),
+    };
+    let options = option_map! {"version" => "2.3.4"};
+    let recipe = tpl
+        .render(&options)
+        .expect("template should not fail to render");
+    assert_eq!(
+        recipe.name(),
+        "my-package",
+        "the default value should be filled in"
+    );
 }
