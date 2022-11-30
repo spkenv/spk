6cfdf4c1f1c20651a93d4d4855ee0adce650521c
David Gilligan-Cook <dcook@imageworks.com>
2022-11-30 12:51:08 -0800

    Fixes bug in search command so it filters out deleted and deprecated packages.
    Adds --deprecated flag to search to show deprecated packages in the output.
    
    Signed-off-by: David Gilligan-Cook <dcook@imageworks.com>

--- crates/spk-cli/group4/src/cmd_search.rs
+++ crates/spk-cli/group4/src/cmd_search.rs
@@ -3,9 +3,11 @@
 // https://github.com/imageworks/spk
 use anyhow::Result;
 use clap::Args;
+use colored::Colorize;
 use spk_cli_common::{flags, CommandArgs, Run};
 use spk_schema::foundation::format::FormatIdent;
 use spk_schema::ident::parse_ident;
+use spk_schema::Deprecate;
 
 /// Search for packages by name/substring
 #[derive(Args)]
@@ -15,6 +17,10 @@ pub struct Search {
 
     #[clap(short, long, global = true, parse(from_occurrences))]
     pub verbose: u32,
+
+    /// Show deprecated packages in the output
+    #[clap(long, short)]
+    deprecated: bool,
 
     /// The text/substring to search for in package names
     term: String,
@@ -41,8 +47,60 @@ impl Run for Search {
                 let versions = repo.list_package_versions(&name).await?;
                 for v in versions.iter() {
                     ident.version = (**v).clone();
+
+                    let builds = repo.list_package_builds(&ident).await?;
+                    if builds.is_empty() {
+                        // A version with no builds is treated as it
+                        // it does not really exist. This can happen
+                        // when a previously published package is
+                        // deleted by 'spk rm'.
+                        continue;
+                    }
+
+                    // Check recipe exists and for deprecation
+                    let mut deprecation_status = "".black();
+                    match repo.read_recipe(&ident).await {
+                        Ok(recipe) => {
+                            if recipe.is_deprecated() {
+                                if self.deprecated {
+                                    deprecation_status = " DEPRECATED".red();
+                                } else {
+                                    // Hide the deprecated ones
+                                    continue;
+                                }
+                            }
+                        }
+                        Err(_) => {
+                            // It doesn't have a recipe, but it does
+                            // have builds, so unless all the builds
+                            // are deprecated, show it. This can
+                            // happen when there is a version of a
+                            // package that only exists as embedded builds.
+                            let mut all_builds_deprecated = true;
+                            for build in builds {
+                                if let Ok(spec) = repo.read_package(&build).await {
+                                    if !spec.is_deprecated() {
+                                        all_builds_deprecated = false;
+                                        break;
+                                    }
+                                }
+                            }
+                            if all_builds_deprecated {
+                                if self.deprecated {
+                                    deprecation_status = " DEPRECATED".red();
+                                } else {
+                                    continue;
+                                }
+                            }
+                        }
+                    };
+
                     exit = 0;
-                    println!("{repo_name: <width$} {}", ident.format_ident());
+                    println!(
+                        "{repo_name: <width$} {}{}",
+                        ident.format_ident(),
+                        deprecation_status
+                    );
                 }
             }
         }
