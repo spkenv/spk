7b80c6b600127b72d8c9a29ee28b8218e4d3d138
Nichol Yip <nyip@wolf1062.spimageworks.com>
2023-02-16 16:08:42 -0800

    Implemented adding size and mode values of file/dirs in spfs ls output Implemented --recursive option for spfs ls that outputs file/dirs in a layer
    Signed-off-by: Nichol Yip <nyip@wolf1062.spimageworks.com>
    
    Fixed linting errors
    
    Signed-off-by: Nichol Yip <nyip@wolf1062.spimageworks.com>
    
    Fixed linting issues
    
    Signed-off-by: Nichol Yip <nyip@wolf1062.spimageworks.com>
    
    Fixed linting errors
    
    Signed-off-by: Nichol Yip <nyip@wolf1062.spimageworks.com>
    
    Sorted normal output
    
    Signed-off-by: Nichol Yip <nyip@wolf1062.spimageworks.com>
    (cherry picked from commit 58f3ea2566903148bed91ba26cb73d46cd311566)
    
    Added -R short alias for --recursive and fixed print format
    
    Signed-off-by: Nichol Yip <nyip@wolf1062.spimageworks.com>
    (cherry picked from commit c512b0888f3312229513f7247d3b63f96402c550)
    
    Re-organized if statements by removing redundant code
    
    Signed-off-by: Nichol Yip <nyip@wolf1062.spimageworks.com>
    (cherry picked from commit 65637749cd9ac1faee4ea68d5fc342dc8ab574fe)

--- crates/spfs-cli/main/src/cmd_ls.rs
+++ crates/spfs-cli/main/src/cmd_ls.rs
@@ -2,8 +2,11 @@
 // SPDX-License-Identifier: Apache-2.0
 // https://github.com/imageworks/spk
 
+use std::collections::HashMap;
+
 use anyhow::Result;
 use clap::Args;
+use itertools::Itertools;
 
 /// List the contents of a committed directory
 #[derive(Debug, Args)]
@@ -16,6 +19,10 @@ pub struct CmdLs {
     /// The tag or digest of the file tree to read from
     #[clap(value_name = "REF")]
     reference: spfs::tracking::EnvSpecItem,
+
+    /// Recursively list all files and directories
+    #[clap(long, short = 'R')]
+    recursive: bool,
 
     /// The subdirectory to list
     #[clap(default_value = "/spfs")]
@@ -34,9 +41,51 @@ impl CmdLs {
             .unwrap_or(&self.path)
             .to_string();
         let manifest = spfs::compute_object_manifest(item, &repo).await?;
-        if let Some(entries) = manifest.list_dir(path.as_str()) {
-            for name in entries {
-                println!("{name}");
+        if let Some(entries) = manifest.list_dir_verbose(path.as_str()) {
+            if self.recursive {
+                if let Some(entries) = manifest.list_dir_verbose(path.as_str()) {
+                    let mut new_entries: HashMap<String, HashMap<String, spfs::tracking::Entry>> =
+                        HashMap::new();
+                    let mut temp_new_entries: HashMap<
+                        String,
+                        HashMap<String, spfs::tracking::Entry>,
+                    > = HashMap::new();
+                    new_entries.insert(".".to_string(), entries);
+                    loop {
+                        if new_entries.is_empty() {
+                            break;
+                        }
+                        for dir in new_entries.keys().sorted() {
+                            println!("{dir}:");
+                            for entry_name in new_entries[dir].keys().sorted() {
+                                if let Some(entry) = new_entries[dir].get(entry_name) {
+                                    match entry.kind {
+                                        spfs::tracking::EntryKind::Tree => {
+                                            print!("{entry_name}/  ");
+                                            temp_new_entries.insert(
+                                                format!("{dir}/{entry_name}"),
+                                                entry.entries.clone(),
+                                            );
+                                        }
+                                        _ => print!("{entry_name}  "),
+                                    }
+                                }
+                            }
+                            println!("\n");
+                        }
+                        new_entries = temp_new_entries.clone();
+                        temp_new_entries.clear()
+                    }
+                }
+            } else {
+                for name in entries.keys().sorted() {
+                    match entries[name].kind {
+                        spfs::tracking::EntryKind::Tree => {
+                            println!("{} {} {}/", entries[name].mode, entries[name].size, name)
+                        }
+                        _ => println!("{} {} {}", entries[name].mode, entries[name].size, name),
+                    }
+                }
             }
         } else {
             match manifest.get_path(path.as_str()) {
