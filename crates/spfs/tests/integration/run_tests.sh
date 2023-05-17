a48d42c489f2c3c1bd8471484e5970dc50f68e34
J Robert Ray <jrray@jrray.org>
2023-05-17 13:22:34 -0700

    Create a reusable test harness for integration tests
    Modify the behavior of the integration tests to run all the tests, pass or
    fail, and then report a summary of if any failed.
    
    Signed-off-by: J Robert Ray <jrray@jrray.org>
    Signed-off-by: Ryan Bottriell <rbottriell@ilm.com>

--- crates/spfs/tests/integration/run_tests.sh
+++ crates/spfs/tests/integration/run_tests.sh
@@ -8,19 +8,6 @@
 # these are expected to be run off of the installed spfs binaries
 # with the proper capabilities
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/unprivileged" &> /dev/null && pwd )"
-for file in $(ls $DIR); do
-  if [[ "$file" == `basename ${BASH_SOURCE[0]}` || "$file" == README ]]; then
-    continue
-  fi
-  echo running test: $file
-  echo "-----------------------------"
-  bash -ex "$DIR/$file"
-  result="$?"
-  sleep 1
-  if [[ "$result" -ne 0 ]]; then
-    echo test failed: $file
-    exit 1;
-  fi
-  echo "----------- OK --------------"
-done
+. $(dirname "${BASH_SOURCE[0]}")/test_harness.sh
+
+run_tests_in_dir $(dirname "${BASH_SOURCE[0]}")/unprivileged
