4f6ebcb31fbf9d96a79912c289c415b9926b19cd
J Robert Ray <jrray@jrray.org>
2023-06-09 20:06:56 -0700

    Add test for nested embedded packages
    This probably shouldn't be allowed, but it currently works.
    
    Signed-off-by: J Robert Ray <jrray@jrray.org>

--- crates/spk-solve/src/solver_test.rs
+++ crates/spk-solve/src/solver_test.rs
@@ -2186,6 +2186,71 @@ async fn test_solver_component_embedded_component_requirements(
 }
 
 #[rstest]
+#[case::comp1(&["mypkg"], true)]
+#[tokio::test]
+async fn test_solver_embedded_embedded_requirements(
+    mut solver: Solver,
+    #[case] packages_to_request: &[&str],
+    #[case] expected_solve_result: bool,
+) {
+    // test a package with an embedded package with an embedded package adds
+    // requirements as expected
+
+    let repo = make_repo!(
+        [
+            {
+                "pkg": "mypkg/1.0.0",
+                "install": {
+                    "embedded": [
+                        {
+                            "pkg": "dep-e1/1.0.0",
+                            "install": {
+                                "components": [
+                                    {
+                                        "name": "run",
+                                        "embedded": [
+                                            {
+                                                "pkg": "dep-e2/1.0.0",
+                                                "install": {
+                                                    "components": [
+                                                        {
+                                                            "name": "run",
+                                                            "requirements": [{"pkg": "dep-e3/1.0.0"}]
+                                                        }
+                                                    ]
+                                                }
+                                            }
+                                        ]
+                                    }
+                                ]
+                            },
+                        },
+                    ],
+                },
+            },
+            {"pkg": "dep-e3/1.0.0"},
+        ]
+    );
+    let repo = Arc::new(repo);
+
+    solver.add_repository(repo);
+    for package_to_request in packages_to_request {
+        solver.add_request(request!(package_to_request));
+    }
+
+    match run_and_print_resolve_for_tests(&solver).await {
+        Ok(solution) => {
+            assert!(expected_solve_result, "expected solve to fail");
+
+            assert_resolved!(solution, "dep-e3", "1.0.0");
+        }
+        Err(_) => {
+            assert!(!expected_solve_result, "expected solve to succeed");
+        }
+    }
+}
+
+#[rstest]
 fn test_solver_get_request_validator() {
     let solver = Solver::default();
     let resolve_validator = solver.request_validator();
