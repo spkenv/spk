0cfcd3718eaec37c7afb413fc21beff3d2c926d9
David Gilligan-Cook <dcook@imageworks.com>
2023-06-29 12:24:25 -0700

    Compile fixes for formatting VarRequest's value field since it changed to a pinnable
    Signed-off-by: David Gilligan-Cook <dcook@imageworks.com>

--- crates/spk-solve/src/io.rs
+++ crates/spk-solve/src/io.rs
@@ -1175,7 +1175,7 @@ impl DecisionFormatter {
             String::from("vars"),
             serde_json::json!(vars
                 .iter()
-                .map(|v| format!("{}: {}", v.var, v.value))
+                .map(|v| format!("{}: {:?}", v.var, v.value))
                 .collect::<Vec<String>>()),
         );
         data.insert(String::from("seconds"), serde_json::json!(seconds));
@@ -1186,7 +1186,7 @@ impl DecisionFormatter {
             "spk explain {} {}",
             requests.join(" "),
             vars.iter()
-                .map(|v| format!("-o {}={}", v.var, v.value))
+                .map(|v| format!("-o {}={:?}", v.var, v.value))
                 .collect::<Vec<String>>()
                 .join(" ")
         );
