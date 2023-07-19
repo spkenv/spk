f1ae8538c8af97d46756f2b6e9c0d0af3748f9a0
J Robert Ray <jrray@jrray.org>
2023-07-18 19:34:46 -0700

    Fix cyclic crate dependency #1
    Move the macros in spk-solve to its own crate, to break the cycle of
    
       spk-solve -> spk-solve-validation -> spk-solve (dev)
    
    The macro code was changed to remove many uses of re-exported symbols
    through `$crate`, since adding these dependencies would reintroduce a
    cyclic dependency.
    
    Signed-off-by: J Robert Ray <jrray@jrray.org>

--- crates/spk-solve/crates/macros/src/lib.rs
+++ crates/spk-solve/crates/macros/src/lib.rs
@@ -2,6 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0
 // https://github.com/imageworks/spk
 
+#![deny(unsafe_op_in_unsafe_fn)]
+#![warn(clippy::fn_params_excessive_bools)]
+
+pub use spk_schema::recipe;
+pub use {serde_json, spfs};
+
 /// Creates a repository containing a set of provided package specs.
 /// It will take care of publishing the spec, and creating a build for
 /// each provided package so that it can be resolved.
@@ -14,16 +20,16 @@ macro_rules! make_repo {
         make_repo!([ $( $spec ),* ], options={})
     }};
     ( [ $( $spec:tt ),+ $(,)? ], options={ $($k:expr => $v:expr),* } ) => {{
-        let options = $crate::option_map!{$($k => $v),*};
+        let options = spk_schema::foundation::option_map!{$($k => $v),*};
         make_repo!([ $( $spec ),* ], options=options)
     }};
     ( [ $( $spec:tt ),+ $(,)? ], options=$options:expr ) => {{
         tracing::debug!("creating in-memory repository");
-        let repo = $crate::RepositoryHandle::new_mem();
+        let repo = spk_storage::RepositoryHandle::new_mem();
         let _opts = $options;
         $(
             let (s, cmpts) = $crate::make_package!(repo, $spec, &_opts);
-            tracing::trace!(pkg=%$crate::Package::ident(&s), cmpts=?cmpts.keys(), "adding package to repo");
+            tracing::trace!(pkg=%spk_schema::Package::ident(&s), cmpts=?cmpts.keys(), "adding package to repo");
             repo.publish_package(&s, &cmpts).await.unwrap();
         )*
         repo
@@ -36,7 +42,7 @@ macro_rules! make_package {
         ($build_spec, $components)
     }};
     ($repo:ident, $build_spec:ident, $opts:expr) => {{
-        use $crate::Package;
+        use spk_schema::Package;
         let s = $build_spec.clone();
         let cmpts: std::collections::HashMap<_, $crate::spfs::encoding::Digest> = s
             .components()
@@ -47,7 +53,7 @@ macro_rules! make_package {
     }};
     ($repo:ident, $spec:tt, $opts:expr) => {{
         let json = $crate::serde_json::json!($spec);
-        let spec: $crate::v0::Spec<$crate::AnyIdent> =
+        let spec: spk_schema::v0::Spec<spk_schema::ident::AnyIdent> =
             $crate::serde_json::from_value(json).expect("Invalid spec json");
         match spec.pkg.build().map(|b| b.clone()) {
             None => {
@@ -55,10 +61,13 @@ macro_rules! make_package {
                 $repo.force_publish_recipe(&recipe).await.unwrap();
                 make_build_and_components!(recipe, [], $opts, [])
             }
-            Some($crate::Build::Source) => {
+            Some(spk_schema::foundation::ident_build::Build::Source) => {
                 let recipe = spec.clone().map_ident(|i| i.into_base());
                 $repo.force_publish_recipe(&recipe.into()).await.unwrap();
-                let build = spec.map_ident(|i| i.into_base().into_build($crate::Build::Source));
+                let build = spec.map_ident(|i| {
+                    i.into_base()
+                        .into_build(spk_schema::foundation::ident_build::Build::Source)
+                });
                 make_build_and_components!(build, [], $opts, [])
             }
             Some(b) => {
@@ -108,31 +117,31 @@ macro_rules! make_build_and_components {
         make_build_and_components!($spec, [$($dep),*], $opts, [])
     };
     ($spec:tt, [$($dep:expr),*], { $($k:expr => $v:expr),* }, [$($component:expr),*]) => {{
-        let opts = $crate::option_map!{$($k => $v),*};
+        let opts = spk_schema::foundation::option_map!{$($k => $v),*};
         make_build_and_components!($spec, [$($dep),*], opts, [$($component),*])
     }};
     ($spec:tt, [$($dep:expr),*], $opts:expr, [$($component:expr),*]) => {{
         #[allow(unused_imports)]
-        use $crate::{Package, Recipe};
+        use spk_schema::{Package, Recipe};
         let json = $crate::serde_json::json!($spec);
-        let spec: $crate::v0::Spec<$crate::AnyIdent> =
+        let spec: spk_schema::v0::Spec<spk_schema::ident::AnyIdent> =
             $crate::serde_json::from_value(json).expect("Invalid spec json");
-        let mut components = std::collections::HashMap::<$crate::Component, $crate::spfs::encoding::Digest>::new();
+        let mut components = std::collections::HashMap::<spk_schema::foundation::ident_component::Component, $crate::spfs::encoding::Digest>::new();
         match spec.pkg.build().map(|b| b.clone()) {
             None => {
                 let recipe = spec.clone().map_ident(|i| i.into_base());
                 let mut build_opts = $opts.clone();
                 #[allow(unused_mut)]
-                let mut solution = $crate::Solution::new(build_opts.clone());
+                let mut solution = spk_solve_solution::Solution::new(build_opts.clone());
                 $(
                 let dep = Arc::new($dep.clone());
                 solution.add(
-                    $crate::PkgRequest::from_ident(
+                    spk_schema::ident::PkgRequest::from_ident(
                         $dep.ident().to_any(),
-                        $crate::RequestedBy::SpkInternalTest,
+                        spk_schema::ident::RequestedBy::SpkInternalTest,
                     ),
                     Arc::clone(&dep),
-                    $crate::PackageSource::SpkInternalTest,
+                    spk_solve_solution::PackageSource::SpkInternalTest,
                 );
                 )*
                 let mut resolved_opts = recipe.resolve_options(&build_opts).unwrap().into_iter();
@@ -145,15 +154,15 @@ macro_rules! make_build_and_components {
                     names = build.components().iter().map(|c| c.name.to_string()).collect();
                 }
                 for name in names {
-                    let name = $crate::Component::parse(name).expect("invalid component name");
+                    let name = spk_schema::foundation::ident_component::Component::parse(name).expect("invalid component name");
                     components.insert(name, $crate::spfs::encoding::EMPTY_DIGEST.into());
                 }
-                ($crate::Spec::V0Package(build), components)
+                (spk_schema::Spec::V0Package(build), components)
             }
-            Some(b @ $crate::Build::Source) => {
+            Some(b @ spk_schema::foundation::ident_build::Build::Source) => {
                 let build = spec.map_ident(|i| i.into_base().into_build(b));
-                components.insert($crate::Component::Source, $crate::spfs::encoding::EMPTY_DIGEST.into());
-                ($crate::Spec::V0Package(build), components)
+                components.insert(spk_schema::foundation::ident_component::Component::Source, $crate::spfs::encoding::EMPTY_DIGEST.into());
+                (spk_schema::Spec::V0Package(build), components)
             }
             Some(b) => {
                 let build = spec.map_ident(|i| i.into_base().into_build(b));
@@ -162,10 +171,10 @@ macro_rules! make_build_and_components {
                     names = build.components().iter().map(|c| c.name.to_string()).collect();
                 }
                 for name in names {
-                    let name = $crate::Component::parse(name).expect("invalid component name");
+                    let name = spk_schema::foundation::ident_component::Component::parse(name).expect("invalid component name");
                     components.insert(name, $crate::spfs::encoding::EMPTY_DIGEST.into());
                 }
-                ($crate::Spec::V0Package(build), components)
+                (spk_schema::Spec::V0Package(build), components)
             }
         }
     }}
@@ -187,20 +196,20 @@ macro_rules! make_spec {
 #[macro_export(local_inner_macros)]
 macro_rules! request {
     ($req:literal) => {
-        $crate::Request::Pkg($crate::PkgRequest::new(
-            $crate::parse_ident_range($req).unwrap(),
-            RequestedBy::SpkInternalTest,
+        spk_schema::ident::Request::Pkg(spk_schema::ident::PkgRequest::new(
+            spk_schema::ident::parse_ident_range($req).unwrap(),
+            spk_schema::ident::RequestedBy::SpkInternalTest,
         ))
     };
     ($req:ident) => {
-        $crate::Request::Pkg($crate::PkgRequest::new(
-            $crate::parse_ident_range($req).unwrap(),
-            RequestedBy::SpkInternalTest,
+        spk_schema::ident::Request::Pkg(spk_schema::ident::PkgRequest::new(
+            spk_schema::ident::parse_ident_range($req).unwrap(),
+            spk_schema::ident::RequestedBy::SpkInternalTest,
         ))
     };
     ($req:tt) => {{
         let value = serde_json::json!($req);
-        let req: $crate::Request = serde_json::from_value(value).unwrap();
+        let req: spk_schema::ident::Request = serde_json::from_value(value).unwrap();
         req
     }};
 }
