3e3ce4b1bbcc85a4a22b4936ffc3b6952e888082
J Robert Ray <jrray@jrray.org>
2023-09-14 17:43:12 -0700

    Add some basic platform tests
    Demonstrate basic platform syntax and expected inheritance behavior.
    
    Signed-off-by: J Robert Ray <jrray@jrray.org>

---
+++ crates/spk-schema/src/v0/platform_test.rs
@@ -0,0 +1,167 @@
+// Copyright (c) Sony Pictures Imageworks, et al.
+// SPDX-License-Identifier: Apache-2.0
+// https://github.com/imageworks/spk
+
+use rstest::rstest;
+use spk_schema_foundation::option_map;
+use spk_schema_ident::{BuildIdent, InclusionPolicy, Request, VersionIdent};
+
+use super::Platform;
+use crate::v0::Spec;
+use crate::{BuildEnv, Recipe};
+
+#[rstest]
+fn test_platform_is_valid_with_only_api_and_name() {
+    let _spec: Platform<VersionIdent> = serde_yaml::from_str(
+        r#"
+         platform: test-platform
+         api: v0/platform
+         "#,
+    )
+    .unwrap();
+}
+
+#[rstest]
+#[case::add_form(
+    r#"
+         platform: test-platform
+         api: v0/platform
+         requirements:
+           - pkg: test-requirement
+         "#
+)]
+#[case::patch_form(
+    r#"
+         platform: test-platform
+         api: v0/platform
+         requirements:
+           add:
+             - pkg: test-requirement
+         "#
+)]
+fn test_platform_add_pkg_requirement(#[case] spec: &str) {
+    struct TestBuildEnv();
+
+    impl BuildEnv for TestBuildEnv {
+        type Package = Spec<BuildIdent>;
+
+        fn build_env(&self) -> Vec<Self::Package> {
+            Vec::new()
+        }
+    }
+
+    let build_env = TestBuildEnv();
+
+    let spec: Platform<VersionIdent> = serde_yaml::from_str(spec).unwrap();
+
+    let build = spec
+        .generate_binary_build(&option_map! {}, &build_env)
+        .unwrap();
+
+    assert_eq!(build.install.requirements.len(), 1);
+    assert!(
+        matches!(&build.install.requirements[0], Request::Pkg(pkg) if pkg.pkg.name() == "test-requirement")
+    );
+    assert!(
+        matches!(&build.install.requirements[0], Request::Pkg(pkg) if pkg.inclusion_policy == InclusionPolicy::IfAlreadyPresent)
+    );
+}
+
+#[rstest]
+fn test_platform_inheritance() {
+    struct TestBuildEnv();
+
+    impl BuildEnv for TestBuildEnv {
+        type Package = Spec<BuildIdent>;
+
+        fn build_env(&self) -> Vec<Self::Package> {
+            vec![serde_yaml::from_str(
+                r#"
+                api: package/v0
+                pkg: base/1.0.0/3TCOOP2W
+                install:
+                  requirements:
+                    - pkg: inherit-me
+            "#,
+            )
+            .unwrap()]
+        }
+    }
+
+    let build_env = TestBuildEnv();
+
+    let spec: Platform<VersionIdent> = serde_yaml::from_str(
+        r#"
+         platform: test-platform
+         base: base
+         api: v0/platform
+         requirements:
+           - pkg: test-requirement
+         "#,
+    )
+    .unwrap();
+
+    let build = spec
+        .generate_binary_build(&option_map! {}, &build_env)
+        .unwrap();
+
+    assert_eq!(build.install.requirements.len(), 2);
+    assert!(
+        matches!(&build.install.requirements[0], Request::Pkg(pkg) if pkg.pkg.name() == "inherit-me")
+    );
+    assert!(
+        matches!(&build.install.requirements[1], Request::Pkg(pkg) if pkg.pkg.name() == "test-requirement")
+    );
+}
+
+#[rstest]
+fn test_platform_inheritance_with_override_and_removal() {
+    struct TestBuildEnv();
+
+    impl BuildEnv for TestBuildEnv {
+        type Package = Spec<BuildIdent>;
+
+        fn build_env(&self) -> Vec<Self::Package> {
+            vec![serde_yaml::from_str(
+                r#"
+                api: package/v0
+                pkg: base/1.0.0/3TCOOP2W
+                install:
+                  requirements:
+                    - pkg: inherit-me1/1.0.0
+                    - pkg: inherit-me2/1.0.0
+                    - pkg: inherit-me3/1.0.0
+            "#,
+            )
+            .unwrap()]
+        }
+    }
+
+    let build_env = TestBuildEnv();
+
+    let spec: Platform<VersionIdent> = serde_yaml::from_str(
+        r#"
+         platform: test-platform
+         base: base
+         api: v0/platform
+         requirements:
+           add:
+             - pkg: inherit-me1/2.0.0
+           remove:
+             - pkg: inherit-me2
+         "#,
+    )
+    .unwrap();
+
+    let build = spec
+        .generate_binary_build(&option_map! {}, &build_env)
+        .unwrap();
+
+    assert_eq!(build.install.requirements.len(), 2);
+    assert!(
+        matches!(&build.install.requirements[0], Request::Pkg(pkg) if pkg.pkg.name() == "inherit-me1" && pkg.pkg.version.to_string() == "2.0.0")
+    );
+    assert!(
+        matches!(&build.install.requirements[1], Request::Pkg(pkg) if pkg.pkg.name() == "inherit-me3")
+    );
+}
