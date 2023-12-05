d314662f5c02e771d8988057f4f745b8b1523c24
J Robert Ray <jrray@jrray.org>
2023-12-05 13:01:41 -0800

    Add test to demonstrate digest collision
    An empty platform, something that will get created in storage when
    starting an empty runtime, happens to have the same digest as a blob
    containing 8 null bytes.
    
    A file with 8 null bytes is something cargo will write in the target
    directory under some circumstances.
    
    Therefore, creating an spfs layer out of the target directory of a Rust
    project can corrupt the spfs storage, as the blob object does not get
    written since the file already exists, and that file is a platform
    object.
    
    This test serves to demonstrate the collision and then eventually verify
    the collision problem has been fixed.
    
    Signed-off-by: J Robert Ray <jrray@jrray.org>
    Signed-off-by: Ryan Bottriell <rbottriell@ilm.com>

---
+++ crates/spfs/tests/integration/unprivileged/test_manifest_digest_collision.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Copyright (c) Sony Pictures Imageworks, et al.
+# SPDX-License-Identifier: Apache-2.0
+# https://github.com/imageworks/spk
+
+set -o errexit
+
+# test that an empty platform's digest doesn't collide with a specially crafted
+# blob
+
+# Commit a blob containing 8 null bytes. This blob will also have the digest
+# V5KXB5NBQEFXV54MV5F4OCTGB4G7KHSCXL4R2TPFWIZI3YHIHX6A====
+spfs run - -- bash -c "dd if=/dev/zero bs=1 count=8 2>/dev/null | spfs write -t test/blob"
+
+# Commit an empty platform. At the time of writing with the current digest
+# calculation strategy, this should produce a digest of
+# V5KXB5NBQEFXV54MV5F4OCTGB4G7KHSCXL4R2TPFWIZI3YHIHX6A====
+spfs run - -- bash -c "spfs commit --allow-empty platform -t test/empty_platform"
+
+# It should be possible to `spfs read` the blob; this command fails if the
+# object is not a blob.
+spfs read test/blob
+
+# Reading the blob should succeed and have the expected contents.
+expected_hash=$(dd if=/dev/zero bs=1 count=8 2>/dev/null | sha256sum | cut -d' ' -f1)
+actual_hash=$(spfs read test/blob 2>/dev/null | sha256sum | cut -d' ' -f1)
+
+if [ "$expected_hash" != "$actual_hash" ]; then
+    echo "Expected hash $expected_hash but got $actual_hash"
+    exit 1
+fi
