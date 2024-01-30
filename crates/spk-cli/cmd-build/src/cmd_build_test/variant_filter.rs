aa2f711d33ce39463fd2740d083c8eb448ddaec8
J Robert Ray <jrray@jrray.org>
2024-01-30 13:17:49 -0800

    Confirm --opts works with a recipe with no variant
    There are no variants to filter out, so the "default" variant should be
    built. Overrides are still applicable.
    
    Signed-off-by: J Robert Ray <jrray@jrray.org>

--- crates/spk-cli/cmd-build/src/cmd_build_test/variant_filter.rs
+++ crates/spk-cli/cmd-build/src/cmd_build_test/variant_filter.rs
@@ -101,3 +101,50 @@ build:
 
     result.expect_err("Expected build to fail");
 }
+
+#[rstest]
+#[tokio::test]
+async fn test_build_with_opts_on_recipe_with_no_variants(tmpdir: tempfile::TempDir) {
+    let _rt = spfs_runtime().await;
+
+    let (_, result) = try_build_package!(
+        tmpdir,
+        "no-variants.spk.yaml",
+        br#"
+pkg: no-variants/1.0.0
+api: v0/package
+build:
+    options:
+        - var: color/blue
+    script:
+        - 'echo "color: $SPK_OPT_color" > "$PREFIX/color.txt"'
+    "#,
+        // By saying --opt color=green, we are asking for a bespoke variant
+        "--opt",
+        "color=green",
+    );
+
+    let mut result = result.expect("Expected build to succeed");
+
+    // Only care about binary builds (not source builds)
+    result
+        .created_builds
+        .artifacts
+        .retain(|(_, artifact)| matches!(artifact, BuildArtifact::Binary(_, _, _)));
+
+    assert_eq!(
+        result.created_builds.artifacts.len(),
+        1,
+        "Expected one build to be created"
+    );
+
+    let opt_name_color = opt_name!("color");
+
+    assert!(
+        matches!(
+            &result.created_builds.artifacts[0].1,
+            BuildArtifact::Binary(_, 0, options) if matches!(options.get(opt_name_color), Some(color) if color == "green")
+        ),
+        "Expected the first variant to be built, and color=green"
+    );
+}
