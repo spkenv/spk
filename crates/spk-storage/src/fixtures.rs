a624aa129e6df5c0db490ff6b08b3055ae634d9c
J Robert Ray <jrray@jrray.org>
2024-02-02 16:51:47 -0800

    Give test repos unique names
    Certain operations in the solver use the repository name as a key, and if
    tests create multiple repos to interact with, they will not be treated as
    different repos if they have the same name.
    
    Signed-off-by: J Robert Ray <jrray@jrray.org>

--- crates/spk-storage/src/fixtures.rs
+++ crates/spk-storage/src/fixtures.rs
@@ -124,7 +124,14 @@ pub async fn make_repo(kind: RepoKind) -> TempRepo {
                 .await
                 .expect("failed to save empty manifest to spfs repo");
             assert_eq!(written, spfs::encoding::EMPTY_DIGEST.into());
-            storage::RepositoryHandle::SPFS(("temp-repo", spfs_repo).try_into().unwrap())
+            storage::RepositoryHandle::SPFS(
+                (
+                    format!("temp-repo-{}", ulid::Ulid::new().to_string().to_lowercase()),
+                    spfs_repo,
+                )
+                    .try_into()
+                    .unwrap(),
+            )
         }
         RepoKind::Mem => storage::RepositoryHandle::new_mem(),
     };
