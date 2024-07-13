b3f67cf8a56b95b406c1b5cc1763f07ebb429e60
Ryan Bottriell <ryan@bottriell.ca>
2024-07-13 14:22:57 -0700

    Replace 'Unlicensed' default with None value
    Signed-off-by: Ryan Bottriell <ryan@bottriell.ca>

--- crates/spk-schema/src/metadata/meta.rs
+++ crates/spk-schema/src/metadata/meta.rs
@@ -14,38 +14,21 @@ use crate::{Error, Result};
 #[path = "./meta_test.rs"]
 mod meta_test;
 
-#[derive(Clone, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+#[derive(Default, Clone, Debug, Deserialize, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
 pub struct Meta {
     #[serde(default, skip_serializing_if = "Option::is_none")]
     pub description: Option<String>,
     #[serde(default, skip_serializing_if = "Option::is_none")]
     pub homepage: Option<String>,
-    #[serde(
-        default = "Meta::default_license",
-        skip_serializing_if = "String::is_empty"
-    )]
-    pub license: String,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub license: Option<String>,
     #[serde(default, skip_serializing_if = "BTreeMap::is_empty")]
     pub labels: BTreeMap<String, String>,
-}
-
-impl Default for Meta {
-    fn default() -> Self {
-        Meta {
-            description: None,
-            homepage: None,
-            license: Self::default_license(),
-            labels: Default::default(),
-        }
-    }
 }
 
 impl Meta {
     pub fn is_default(&self) -> bool {
         self == &Self::default()
-    }
-    fn default_license() -> String {
-        "Unlicensed".into()
     }
 
     pub fn has_label_with_value(&self, label: &str, value: &str) -> bool {
