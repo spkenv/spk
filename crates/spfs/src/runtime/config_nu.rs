51a012d5816a1de2d59a32f8111c30d8a8fc9852
Philippe Llerena <philippe.llerena@gmail.com>
2024-08-09 09:35:48 +0200

    add nushell support start 0.97.0+
    Signed-off-by: Philippe Llerena <philippe.llerena@gmail.com>

---
+++ crates/spfs/src/runtime/config_nu.rs
@@ -0,0 +1,13 @@
+// Copyright (c) Contributors to the SPK project.
+// SPDX-License-Identifier: Apache-2.0
+// https://github.com/spkenv/spk
+// Warning Nuhshell version >=0.96
+
+use std::fs;
+
+pub fn source<T>(_tmpdir: Option<&T>) -> String
+where
+    T: AsRef<str>,
+{
+    fs::read_to_string("/home/philippe.llerena/workspace/github.com/doubleailes/spk/crates/spfs/src/runtime/config.nu").unwrap()
+}
