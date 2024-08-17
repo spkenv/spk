483ae1360179cfabf6373d4613307c17d4c4afb1
Philippe Llerena <philippe.llerena@gmail.com>
2024-08-17 13:58:50 +0200

    Clean config
    Signed-off-by: Philippe Llerena <philippe.llerena@gmail.com>

--- crates/spfs/src/runtime/config.nu
+++
@@ -1,4 +0,0 @@
-$env.config = {
-  show_banner: false,
-}
-print $env.SPFS_SHELL_MESSAGE?
--- crates/spfs/src/runtime/config_nu.rs
+++ crates/spfs/src/runtime/config_nu.rs
@@ -3,11 +3,17 @@
 // https://github.com/spkenv/spk
 // Warning Nuhshell version >=0.96
 
-use std::fs;
 
 pub fn source<T>(_tmpdir: Option<&T>) -> String
 where
     T: AsRef<str>,
-{
-    fs::read_to_string("/home/philippe.llerena/workspace/github.com/doubleailes/spk/crates/spfs/src/runtime/config.nu").unwrap()
-}
+    {
+        r#"
+        $env.config = {
+        show_banner: false,
+        }
+        $env.SPFS_SHELL_MESSAGE? | print
+    "#
+        .to_string()
+    }
+    
