55fe133fe7aefc0269da42d5374783d539ff1f46
David Gilligan-Cook <dcook@imageworks.com>
2024-08-19 11:37:46 -0700

    Adds repo name to 'opening repository' debug messages
    Signed-off-by: David Gilligan-Cook <dcook@imageworks.com>

--- crates/spfs/src/config.rs
+++ crates/spfs/src/config.rs
@@ -587,11 +587,11 @@ impl Config {
         match self.remote.get(remote_name.as_ref()) {
             Some(Remote::Address(remote)) => {
                 let config = RemoteConfig::from_address(remote.address.clone()).await?;
-                tracing::debug!(?config, "opening repository");
+                tracing::debug!(?config, "opening '{}' repository", remote_name.as_ref());
                 config.open().await
             }
             Some(Remote::Config(config)) => {
-                tracing::debug!(?config, "opening repository");
+                tracing::debug!(?config, "opening '{}' repository", remote_name.as_ref());
                 config.open().await
             }
             None => {
