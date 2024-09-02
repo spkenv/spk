e66c0cca3075aee1df579a9216a39087c2053e74
Philippe Llerena <philippe.llerena@gmail.com>
2024-09-02 14:19:54 +0200

    quality: run cargo fmt

--- crates/spfs/src/runtime/config_nu.rs
+++ crates/spfs/src/runtime/config_nu.rs
@@ -3,17 +3,15 @@
 // https://github.com/spkenv/spk
 // Warning Nushell version >=0.97
 
-
 pub fn source<T>(_tmpdir: Option<&T>) -> String
 where
     T: AsRef<str>,
-    {
-        r#"
+{
+    r#"
         $env.config = {
         show_banner: false,
         }
         $env.SPFS_SHELL_MESSAGE? | print
     "#
-        .to_string()
-    }
-    
+    .to_string()
+}
--- crates/spfs/src/runtime/mod.rs
+++ crates/spfs/src/runtime/mod.rs
@@ -21,8 +21,21 @@ pub mod winfsp;
 #[cfg(unix)]
 pub use overlayfs::is_removed_entry;
 pub use storage::{
-    makedirs_with_perms, Author, BindMount, Config, Data, KeyValuePair, KeyValuePairBuf, LiveLayer,
-    LiveLayerFile, MountBackend, OwnedRuntime, Runtime, Status, Storage, STARTUP_FILES_LOCATION,
+    makedirs_with_perms,
+    Author,
+    BindMount,
+    Config,
+    Data,
+    KeyValuePair,
+    KeyValuePairBuf,
+    LiveLayer,
+    LiveLayerFile,
+    MountBackend,
+    OwnedRuntime,
+    Runtime,
+    Status,
+    Storage,
+    STARTUP_FILES_LOCATION,
 };
 #[cfg(windows)]
 pub use winfsp::is_removed_entry;
