67fa96c1a3b4e5ab85ef1acb31e4f633d49aac4f
Nichol Yip <nyip@imageworks.com>
2024-09-09 15:46:22 -0700

    Implemented linting for build options
    Signed-off-by: Nichol Yip <nyip@imageworks.com>

--- crates/spk-schema/src/lints.rs
+++ crates/spk-schema/src/lints.rs
@@ -1,6 +1,6 @@
-// Copyright (c) Sony Pictures Imageworks, et al.
+// Copyright (c) Contributors to the SPK project.
 // SPDX-License-Identifier: Apache-2.0
-// https://github.com/imageworks/spk
+// https://github.com/spkenv/spk
 
 use std::sync::Arc;
 
