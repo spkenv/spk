e04c7e97dc44aa7018de81d074b8c9c5c00459cb
J Robert Ray <jrray@jrray.org>
2025-01-31 14:56:07 -0800

    Implement candidate sorting
    Signed-off-by: J Robert Ray <jrray@jrray.org>

--- crates/spk-solve/src/solvers/resolvo/resolvo_tests.rs
+++ crates/spk-solve/src/solvers/resolvo/resolvo_tests.rs
@@ -5,6 +5,7 @@
 use std::borrow::Cow;
 
 use rstest::rstest;
+use spk_schema::prelude::HasVersion;
 use spk_solve_macros::{make_repo, request};
 
 use super::Solver;
@@ -21,4 +22,24 @@ async fn basic() {
     let mut solver = Solver::new(vec![repo.into()], Cow::Borrowed(&[]));
     let solution = solver.solve(&[request!("basic")]).await.unwrap();
     assert_eq!(solution.len(), 1);
+}
+
+#[rstest]
+#[tokio::test]
+async fn two_choices() {
+    let repo = make_repo!(
+        [
+            {"pkg": "basic/2.0.0"},
+            {"pkg": "basic/1.0.0"},
+        ]
+    );
+
+    let mut solver = Solver::new(vec![repo.into()], Cow::Borrowed(&[]));
+    let solution = solver.solve(&[request!("basic")]).await.unwrap();
+    assert_eq!(solution.len(), 1);
+    // All things being equal it should pick the higher version
+    assert_eq!(
+        solution.items().next().unwrap().spec.version().to_string(),
+        "2.0.0"
+    );
 }
