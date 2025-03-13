9b8bce84b55cd1626b36f9ab18509d06a4e9389f
J Robert Ray <jrray@jrray.org>
2025-03-13 11:17:14 -0700

    Add tests to demonstrate workspace problems
    The new workspace feature has introduced a variety of regressions for
    using `spk info --variants <filename>` and will error with a file not
    found error for files that do actually exist.
    
    One problem is that when a workspace spec doesn't exist, then the only
    possible files that can be inspected have to exist in the root of the
    virtual workspace generated, ignoring that the command line contains the
    name of a valid file.
    
    Another problem is when the workspace does exist, then the only possible
    files that can be inspected have to be defined in the workspace, even
    when the command line contains the name of a valid file.
    
    Perhaps the design can be reconsidered to not use a "virtual workspace"
    in these cases or at least have a fallback that tries to open the file
    directly.
    
    Signed-off-by: J Robert Ray <jrray@jrray.org>

--- crates/spk-cli/group4/src/cmd_view_test.rs
+++ crates/spk-cli/group4/src/cmd_view_test.rs
@@ -7,6 +7,7 @@ use std::io::Write;
 
 use clap::Parser;
 use rstest::{fixture, rstest};
+use spfs::runtime::makedirs_with_perms;
 use spk_cli_common::Run;
 
 use super::View;
@@ -33,6 +34,12 @@ pub fn init_logging() {
         .finish();
     let _ = tracing::subscriber::set_global_default(sub);
 }
+
+// The use of the --workspace flag in these tests is meant to simulate the
+// default behavior without the flag, which defaults to using ".", but these
+// tests want to avoid changing the current working directory during test
+// execution, so the tests don't need to be serialized or have to worry about
+// changing the current working directory back to the original value.
 
 #[rstest]
 #[tokio::test]
@@ -64,4 +71,162 @@ api: v0/package
     ])
     .unwrap();
     opt.view.run().await.unwrap();
+}
+
+#[rstest]
+#[tokio::test]
+async fn view_on_filename_in_sibling_dir_and_default_workspace(
+    #[from(tmpdir)] tmpdir1: tempfile::TempDir,
+    #[from(tmpdir)] tmpdir2: tempfile::TempDir,
+) {
+    init_logging();
+
+    let full_name = tmpdir1.path().join("package.spk.yaml");
+
+    let mut file = File::create(&full_name).unwrap();
+    file.write_all(
+        r#"
+pkg: test/1.0.0
+api: v0/package
+"#
+        .as_bytes(),
+    )
+    .unwrap();
+
+    let mut opt = Opt::try_parse_from([
+        "view",
+        "-vvv",
+        "--workspace",
+        &tmpdir2.path().to_string_lossy(),
+        // A straight `spk info package.spk.yaml` fails with "Failed to parse
+        // request" and/or "yaml was expected to contain a list of requests"
+        // but using the `--variants` flag still does something expected.
+        "--variants",
+        &full_name.to_string_lossy(),
+    ])
+    .unwrap();
+    opt.view.run().await.unwrap();
+}
+
+#[rstest]
+#[tokio::test]
+async fn view_on_filename_in_subdir_and_default_workspace(tmpdir: tempfile::TempDir) {
+    init_logging();
+
+    makedirs_with_perms(tmpdir.path().join("subdir"), 0o777).unwrap();
+
+    let full_name = tmpdir.path().join("subdir").join("package.spk.yaml");
+
+    let mut file = File::create(&full_name).unwrap();
+    file.write_all(
+        r#"
+pkg: test/1.0.0
+api: v0/package
+"#
+        .as_bytes(),
+    )
+    .unwrap();
+
+    let mut opt = Opt::try_parse_from([
+        "view",
+        "-vvv",
+        "--workspace",
+        &tmpdir.path().to_string_lossy(),
+        // A straight `spk info package.spk.yaml` fails with "Failed to parse
+        // request" and/or "yaml was expected to contain a list of requests"
+        // but using the `--variants` flag still does something expected.
+        "--variants",
+        &full_name.to_string_lossy(),
+    ])
+    .unwrap();
+    opt.view.run().await.unwrap();
+}
+
+#[rstest]
+#[tokio::test]
+async fn view_on_filename_and_unrelated_workspace(tmpdir: tempfile::TempDir) {
+    init_logging();
+
+    let full_name = tmpdir.path().join("package.spk.yaml");
+
+    let mut file = File::create(&full_name).unwrap();
+    file.write_all(
+        r#"
+pkg: test/1.0.0
+api: v0/package
+"#
+        .as_bytes(),
+    )
+    .unwrap();
+
+    // This workspace intentionally does not reference package.spk.yaml.
+    let mut file = File::create(tmpdir.path().join("workspace.spk.yaml")).unwrap();
+    file.write_all(
+        r#"
+api: v0/workspace
+recipes: []
+"#
+        .as_bytes(),
+    )
+    .unwrap();
+
+    let mut opt = Opt::try_parse_from([
+        "view",
+        "-vvv",
+        "--workspace",
+        &tmpdir.path().to_string_lossy(),
+        // A straight `spk info package.spk.yaml` fails with "Failed to parse
+        // request" and/or "yaml was expected to contain a list of requests"
+        // but using the `--variants` flag still does something expected.
+        "--variants",
+        &full_name.to_string_lossy(),
+    ])
+    .unwrap();
+    opt.view.run().await.unwrap();
+}
+
+#[rstest]
+#[tokio::test]
+async fn view_on_workspace_filename_and_existing_workspace(tmpdir: tempfile::TempDir) {
+    init_logging();
+
+    let full_name = tmpdir.path().join("workspace.spk.yaml");
+
+    let mut file = File::create(&full_name).unwrap();
+    file.write_all(
+        r#"
+api: v0/workspace
+recipes: []
+"#
+        .as_bytes(),
+    )
+    .unwrap();
+
+    let mut opt = Opt::try_parse_from([
+        "view",
+        "-vvv",
+        "--workspace",
+        &tmpdir.path().to_string_lossy(),
+        // A straight `spk info package.spk.yaml` fails with "Failed to parse
+        // request" and/or "yaml was expected to contain a list of requests"
+        // but using the `--variants` flag still does something expected.
+        //
+        // Doing `--variants` on a workspace file is meaningless but this test
+        // demonstrates how this fails with a "file does not exist" error when
+        // the file does exist.
+        "--variants",
+        &full_name.to_string_lossy(),
+    ])
+    .unwrap();
+    let err = opt
+        .view
+        .run()
+        .await
+        .expect_err("--variants should fail on a workspace file");
+
+    assert!(
+        !err.to_string().contains("did not find package template"),
+        "Expected error to not contain 'did not find package template', got: {}",
+        err,
+    );
 }
