d22ee9816ccaec3b8ad4b7e275b382f4a5d1662d
Bryce Gattis <brycegattis@yahoo.com>
2025-05-28 07:32:37 -0500

    Update cmd docs so we don't have to pipe output manually in the shell command we call
    Signed-off-by: Bryce Gattis <brycegattis@yahoo.com>

--- crates/spfs-cli/main/src/cmd_docs.rs
+++ crates/spfs-cli/main/src/cmd_docs.rs
@@ -1,6 +1,7 @@
 // Copyright (c) Contributors to the SPK project.
 // SPDX-License-Identifier: Apache-2.0
 // https://github.com/spkenv/spk
+use std::fs;
 
 use clap::Args;
 use miette::{IntoDiagnostic, Result};
@@ -11,7 +12,14 @@ pub struct CmdDocs {}
 
 impl CmdDocs {
     pub async fn run(&mut self, config: &spfs::Config) -> Result<i32> {
-        clap_markdown::print_help_markdown::<crate::Opt>();
+        let mut markdown = clap_markdown::help_markdown::<crate::Opt>();
+        markdown.insert_str(0, "\n");
+        markdown.insert_str(0, "---\n");
+        markdown.insert_str(0, "chapter: true\n");
+        markdown.insert_str(0, "title: SPFS CLI\n");
+        markdown.insert_str(0, "---\n");
+        // This path is relative to the current shell directory.
+        fs::write("docs/spfs/cli/markdown.md", markdown).expect("Unable to write file");
         Ok(0)
     }
 }
