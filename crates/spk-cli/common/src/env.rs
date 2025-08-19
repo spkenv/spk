8c00e53ebf275e7c390310a8be6211eb4071abeb
David Gilligan-Cook <dcook@imageworks.com>
2025-08-18 17:27:38 -0700

    Updates requested by list in yaml/json formatted output to split out the name, version, and build of the entries
    Signed-off-by: David Gilligan-Cook <dcook@imageworks.com>

--- crates/spk-cli/common/src/env.rs
+++ crates/spk-cli/common/src/env.rs
@@ -281,10 +281,9 @@ fn get_spk_context() -> (
 }
 
 /// Utility for removing ansi-colour/terminal escape codes from a String
-fn remove_ansi_escapes(message: String) -> String {
-    if let Ok(b) = strip_ansi_escapes::strip(message.clone())
-        && let Ok(s) = std::str::from_utf8(&b)
-    {
+pub fn remove_ansi_escapes(message: String) -> String {
+    let b = strip_ansi_escapes::strip(message.clone());
+    if let Ok(s) = std::str::from_utf8(&b) {
         return s.to_string();
     }
 
