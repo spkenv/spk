2a5e4f587884f71cc7203c916d525a899300cc25
Ryan Bottriell <rbottriell@ilm.com>
2025-09-29 16:19:51 -0700

    Fix numbers not supported in platform v1 requirements
    Signed-off-by: Ryan Bottriell <rbottriell@ilm.com>

--- crates/spk-schema/src/v1/platform.rs
+++ crates/spk-schema/src/v1/platform.rs
@@ -521,6 +521,27 @@ mod value_or_false {
                 ))
             }
 
+            fn visit_i64<E>(self, v: i64) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                self.visit_str(&v.to_string())
+            }
+
+            fn visit_u64<E>(self, v: u64) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                self.visit_str(&v.to_string())
+            }
+
+            fn visit_f64<E>(self, v: f64) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                self.visit_str(&v.to_string())
+            }
+
             fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
             where
                 E: serde::de::Error,
