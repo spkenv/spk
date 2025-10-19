d705c9b32227586bfec3e5e06b8d71c8b137f2c1
J Robert Ray <jrray@jrray.org>
2025-10-18 17:24:47 -0700

    Add a lock-free example that fixes clean races
    UPDATE: this approach still has a race but the original version wasn't
    testing the condition where the race exists. The test has been updated
    to show that a race still exists.
    
    This approach doesn't require there to be any "global" lock while
    performing a clean, but the trade-off is there is more overhead for
    writing objects and for performing a clean.
    
    In summary, when writing an object, first a <digest>.hold file will be
    written. The existence of this file prevents <digest> from being
    garbage.
    
    The <digest>.hold file is not removed until after a tag exists that
    creates a strong reference to the object.
    
    The clean process collects what it thinks are garbage objects as usual,
    but then it checks for .hold files for each candidate garbage object.
    Then, a second pass over all the tags is needed to discover any strong
    references that didn't exist in the first pass.
    
    Since looking for tags happens after checking for .hold files, the new
    tags are guaranteed to be found. The tag must exist by the time the
    .hold file is deleted.
    
    Signed-off-by: J Robert Ray <jrray@jrray.org>

--- clean-loom/src/main.rs
+++ clean-loom/src/main.rs
@@ -2,6 +2,7 @@ use std::collections::BTreeSet;
 
 use loom::sync::{Arc, Mutex};
 use loom::thread;
+use rstest::rstest;
 
 #[test]
 fn simulate_existing_clean_behavior() {
@@ -181,6 +182,115 @@ fn simulate_proposed_clean_behavior_version_2() {
 
         let lock = objects.lock().unwrap();
         assert_eq!(*lock, BTreeSet::<i32>::from_iter([2, 3, 4]));
+    });
+}
+
+#[rstest]
+#[case::write_new(4)]
+#[case::write_existing(1)]
+fn simulate_proposed_lock_free_clean_behavior(
+    #[case] value_to_write: i32,
+    #[values(true, false)] tags_first: bool,
+) {
+    loom::model(move || {
+        // Start out with `1` already as garbage.
+        let tags = Arc::new(Mutex::new(BTreeSet::<i32>::from_iter([2, 3])));
+        let objects = Arc::new(Mutex::new(BTreeSet::<i32>::from_iter([1, 2, 3])));
+        let staged = Arc::new(Mutex::new(BTreeSet::<i32>::new()));
+
+        let writer_tags = Arc::clone(&tags);
+        let writer_objects = Arc::clone(&objects);
+        let writer_staged = Arc::clone(&staged);
+        let writer_thread = thread::spawn(move || {
+            // Before writing any objects, a writer must create a hard reference
+            // to them by "staging them":
+            let mut lock = writer_staged.lock().unwrap();
+            lock.insert(value_to_write);
+            drop(lock);
+            // Now it is safe to add to objects.
+            let mut lock = writer_objects.lock().unwrap();
+            lock.insert(value_to_write);
+            drop(lock);
+            let mut lock = writer_tags.lock().unwrap();
+            lock.insert(value_to_write);
+            drop(lock);
+            // With the tag written, it is now safe to unstage the object.
+            let mut lock = writer_staged.lock().unwrap();
+            lock.remove(&value_to_write);
+        });
+
+        let cleaner_tags = Arc::clone(&tags);
+        let cleaner_objects = Arc::clone(&objects);
+        let cleaner_staged = Arc::clone(&staged);
+        let cleaner_thread = thread::spawn(move || {
+            // This version doesn't have a global lock while deleting objects.
+            // It makes two passes on the objects. The first pass determines
+            // which objects may be garbage. The second pass verifies all
+            // objects in the first list are still garbage.
+
+            // Simulate reading tags and objects in either order.
+            // Running two test variants is vastly faster than using threads to
+            // simulate the order being non-deterministic.
+            let (objects_snapshot, tags_snapshot) = if tags_first {
+                let lock = cleaner_tags.lock().unwrap();
+                let _tags_snapshot = (*lock).clone();
+                drop(lock);
+                let lock = cleaner_objects.lock().unwrap();
+                let _objects_snapshot = (*lock).clone();
+                drop(lock);
+                (_objects_snapshot, _tags_snapshot)
+            } else {
+                let lock = cleaner_objects.lock().unwrap();
+                let _objects_snapshot = (*lock).clone();
+                drop(lock);
+                let lock = cleaner_tags.lock().unwrap();
+                let _tags_snapshot = (*lock).clone();
+                drop(lock);
+                (_objects_snapshot, _tags_snapshot)
+            };
+
+            let to_delete = objects_snapshot
+                .difference(&tags_snapshot)
+                .copied()
+                .collect::<BTreeSet<_>>();
+
+            // Disqualify any objects that (still) have a staging file.
+            let staged_snapshot = cleaner_staged.lock().unwrap().clone();
+            let to_delete = to_delete
+                .difference(&staged_snapshot)
+                .copied()
+                .collect::<BTreeSet<_>>();
+            // Finally, re-check the tags to ensure nothing new has been added
+            // since we took our first snapshot.
+            //
+            // The following claim is only true when writing a new object. It is
+            // not true if a writer is introducing a new hard reference to an
+            // existing object that is currently considered garbage.
+            //
+            // It is impossible to not see either the tag or the staging file
+            // for any non-garbage object. Since the tag is written before
+            // deleting the staging file, and the staging file is written before
+            // the object, if we know about the object then one of the two must
+            // exist. We can't check for both in the same pass because we could
+            // end up not seeing a just-written tag and miss seeing the just-
+            // deleted staging file. In practice we only need to walk manifests
+            // that were not seen in the original pass.
+            let tags_2nd_snapshot = cleaner_tags.lock().unwrap().clone();
+            let to_delete = to_delete
+                .difference(&tags_2nd_snapshot)
+                .copied()
+                .collect::<BTreeSet<_>>();
+            for obj in to_delete {
+                let mut lock = cleaner_objects.lock().unwrap();
+                lock.remove(&obj);
+            }
+        });
+
+        cleaner_thread.join().unwrap();
+        writer_thread.join().unwrap();
+
+        let lock = objects.lock().unwrap();
+        assert_eq!(*lock, BTreeSet::<i32>::from_iter([value_to_write, 2, 3]));
     });
 }
 
