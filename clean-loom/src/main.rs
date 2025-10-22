386dc32eef4aa95f756164583b3826e8fbc0d41e
J Robert Ray <jrray@jrray.org>
2025-10-22 13:28:51 -0700

    Yet another simulation but model object hierarchies
    This one tries to detect any deletes that produce a dangling reference.
    It uses the concept of staging again and a "global" lock, but it has a
    nice property that it blocks the cleaner instead of blocking the writer,
    at least in the sense that the lock doesn't have to be continuously held
    by the cleaner. It would also be safe for concurrent writers, but the
    cleaner can only make progress while there are no writers.
    
    However there are some onerous requirements to make this work in
    practice. The whole hierarchy of objects intended to be written need to
    be marked as staged. It isn't safe to see that some sub-hierarchy
    already exists in the destination and skip staging it, because it may
    already be in the process of being deleted.
    
    The cleaner is required to visit/delete objects in top-down order.
    
    The cases added here are passing but more work is needed to exhaustively
    test this approach.
    
    Signed-off-by: J Robert Ray <jrray@jrray.org>

--- clean-loom/src/main.rs
+++ clean-loom/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeSet;
+use std::collections::{BTreeMap, BTreeSet};
 
 use loom::sync::{Arc, Mutex};
 use loom::thread;
@@ -421,6 +421,159 @@ fn simulate_proposed_lock_per_object_clean_behavior(
         assert_eq!(*lock, BTreeSet::<i32>::from_iter([value_to_write, 2, 3]));
     });
 }
+
+#[rstest]
+#[case::write_new_order_67(
+    &[(6, vec![]), (7, vec![]), (8, vec![6, 7])],
+    &[8],
+    &[(6, vec![]), (7, vec![]), (8, vec![6, 7])])
+]
+#[case::write_new_order_76(
+    &[(7, vec![]), (6, vec![]), (8, vec![6, 7])],
+    &[8],
+    &[(6, vec![]), (7, vec![]), (8, vec![6, 7])])
+]
+#[case::reuse_existing_5(
+    // Although reusing existing content, this must include the whole hierarchy.
+    // Trying to get the existing hierarchy from the object store doesn't work
+    // because the cleaner may have already deleted some of it.
+    &[(6, vec![5]), (5, vec![4, 3]), (4, vec![2]), (3, vec![2, 1]), (2, vec![]), (1, vec![])],
+    &[6],
+    &[(6, vec![5]), (5, vec![4, 3]), (4, vec![2]), (3, vec![2, 1]), (2, vec![]), (1, vec![])])
+]
+#[case::reuse_existing_3(
+    &[(6, vec![3]), (3, vec![2, 1]), (2, vec![]), (1, vec![])],
+    &[6],
+    // 5 and 4 may be deleted
+    &[(6, vec![3]), (3, vec![2, 1]), (2, vec![]), (1, vec![])])
+]
+fn simulate_proposed_clean_behavior_with_hierarchy(
+    #[case] values_to_write: &[(i32, Vec<i32>)],
+    #[case] tags_to_write: &[i32],
+    #[case] expected_outcome: &[(i32, Vec<i32>)],
+    #[values(true, false)] tags_first: bool,
+) {
+    let values_to_write = values_to_write.to_owned();
+    let tags_to_write = tags_to_write.to_owned();
+    let expected_outcome = expected_outcome.to_owned();
+
+    loom::model(move || {
+        // This repo models objects that exist in a parent-child hierarchy:
+        //       5
+        //      / \
+        //     4   3
+        //      \ / \
+        //       2   1
+        // Objects must be written from the bottom up; a parent cannot exist
+        // before its children. The node numbers have been assigned to follow
+        // this rule when written in ascending order.
+        // It is not legal to delete a child object if its parent still exists.
+
+        // Start out with everything already as garbage.
+        let tags = Arc::new(Mutex::new(BTreeSet::<i32>::new()));
+        let objects = Arc::new(Mutex::new(BTreeMap::<i32, BTreeSet<i32>>::from_iter([
+            (5, BTreeSet::from_iter([4, 3])),
+            (4, BTreeSet::from_iter([2])),
+            (3, BTreeSet::from_iter([2, 1])),
+            (2, BTreeSet::new()),
+            (1, BTreeSet::new()),
+        ])));
+        let staged = Arc::new(Mutex::new(BTreeSet::<i32>::new()));
+
+        let writer_tags = Arc::clone(&tags);
+        let writer_objects = Arc::clone(&objects);
+        let writer_values_to_write = values_to_write.clone();
+        let writer_tags_to_write = tags_to_write.clone();
+        let writer_staged = Arc::clone(&staged);
+        let writer_thread = thread::spawn(move || {
+            // Writer must stage all objects before writing any of them.
+            {
+                let mut lock = writer_staged.lock().unwrap();
+                for (value, _children) in &writer_values_to_write {
+                    lock.insert(*value);
+                }
+            }
+
+            for (value, children) in writer_values_to_write {
+                let mut lock = writer_objects.lock().unwrap();
+                lock.insert(value, BTreeSet::from_iter(children.iter().copied()));
+            }
+            for tag in writer_tags_to_write {
+                let mut lock = writer_tags.lock().unwrap();
+                lock.insert(tag);
+            }
+        });
+
+        let cleaner_tags = Arc::clone(&tags);
+        let cleaner_objects = Arc::clone(&objects);
+        let cleaner_staged = Arc::clone(&staged);
+        let cleaner_thread = thread::spawn(move || {
+            // Simulate reading tags and objects in either order.
+            // Running two test variants is vastly faster than using threads to
+            // simulate the order being non-deterministic.
+
+            let (objects_snapshot, tags_snapshot) = if tags_first {
+                let lock = cleaner_tags.lock().unwrap();
+                let _tags_snapshot = (*lock).clone();
+                drop(lock);
+                let lock = cleaner_objects.lock().unwrap();
+                let _objects_snapshot = (*lock).clone();
+                drop(lock);
+                (_objects_snapshot, _tags_snapshot)
+            } else {
+                let lock = cleaner_objects.lock().unwrap();
+                let _objects_snapshot = (*lock).clone();
+                drop(lock);
+                let lock = cleaner_tags.lock().unwrap();
+                let _tags_snapshot = (*lock).clone();
+                drop(lock);
+                (_objects_snapshot, _tags_snapshot)
+            };
+
+            let to_delete = objects_snapshot
+                .keys()
+                .copied()
+                .collect::<BTreeSet<_>>()
+                .difference(&tags_snapshot)
+                .copied()
+                .collect::<BTreeSet<_>>();
+
+            // Delete from the top down to prevent violating parent-child
+            // relationships.
+            for obj in to_delete.into_iter().collect::<Vec<_>>().into_iter().rev() {
+                // Hold the staged lock while checking if the object is staged,
+                // through deleting the object.
+                let staged_lock = cleaner_staged.lock().unwrap();
+                if staged_lock.contains(&obj) {
+                    // Still staged, can't delete.
+                    continue;
+                }
+                let mut lock = cleaner_objects.lock().unwrap();
+                lock.remove(&obj);
+                // all remaining objects must have valid children
+                for (parent, children) in lock.iter() {
+                    for child in children {
+                        assert!(
+                            lock.contains_key(child),
+                            "After deleting {obj}, parent {parent} has missing child {child}"
+                        );
+                    }
+                }
+            }
+        });
+
+        cleaner_thread.join().unwrap();
+        writer_thread.join().unwrap();
+
+        let expected_map = expected_outcome
+            .iter()
+            .map(|(k, v)| (*k, BTreeSet::from_iter(v.iter().copied())))
+            .collect::<BTreeMap<_, _>>();
+        let lock = objects.lock().unwrap();
+        assert_eq!(*lock, expected_map);
+    });
+}
+
 fn main() {
     println!("Hello, world!");
 }
