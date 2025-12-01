14515f13e308cd09f5a6b7570bd6f4480078d82b
J Robert Ray <jrray@jrray.org>
2025-12-01 10:16:29 -0800

    Split BuildSpec into BuildSpec and RecipeBuildSpec
    Recipes can contain variants but packages are builds of a single
    variant, so their specs are meant to contain all the resolved options
    used during the build.
    
    Signed-off-by: J Robert Ray <jrray@jrray.org>

--- crates/spk-schema/src/v0/package_spec.rs
+++ crates/spk-schema/src/v0/package_spec.rs
@@ -174,7 +174,7 @@ impl PackageSpec {
             recipe.compat,
             recipe.deprecated,
             recipe.sources,
-            recipe.build,
+            recipe.build.into(),
             recipe.tests,
             InstallSpec::default(),
         );
