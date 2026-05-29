cb46c5e59065199efdb80954e367c961c1e8f2ba
David Gilligan-Cook <dcook@imageworks.com>
2026-05-29 15:37:27 -0700

    Refactors the duplicate methods into a shared function used by both v0 package objects.
    Signed-off-by: David Gilligan-Cook <dcook@imageworks.com>

--- crates/spk-schema/src/v0/indexed_package.rs
+++ crates/spk-schema/src/v0/indexed_package.rs
@@ -16,7 +16,6 @@ use spk_schema_foundation::ident::{
     PinnedValue,
     PkgRequestWithOptions,
     RequestWithOptions,
-    RequestedBy,
 };
 use spk_schema_foundation::ident_build::Build;
 use spk_schema_foundation::ident_component::Component;
@@ -26,6 +25,7 @@ use spk_schema_foundation::spec_ops::HasBuildIdent;
 use spk_schema_foundation::version::{IncompatibleReason, VarOptionProblem};
 
 use super::check_package_spec_satisfies_pkg_request;
+use super::package_spec::build_requirements_from_build_options;
 use crate::fb_converter::fb_requirements_to_requirements;
 use crate::foundation::name::PkgName;
 use crate::foundation::spec_ops::prelude::*;
@@ -497,34 +497,8 @@ impl Package for IndexedPackage {
     }
 
     fn get_build_requirements(&self) -> crate::Result<Cow<'_, RequirementsList<PinnedRequest>>> {
-        let mut requests = RequirementsList::default();
-        // A duplicated of get_build_requirements() from
-        // v0/package_spec with this line changed to call a method.
-        for opt in self.get_build_options().iter() {
-            match opt {
-                Opt::Pkg(opt) => {
-                    let mut req =
-                        opt.to_request(None, RequestedBy::BinaryBuild(self.ident().clone()))?;
-                    if req.pkg.components.is_empty() {
-                        // inject the default component for this context if needed
-                        req.pkg.components.insert(Component::default_for_build());
-                    }
-                    requests.insert_or_merge_pinned(PinnedRequest::Pkg(req))?;
-                }
-                Opt::Var(opt) => {
-                    // If no value was specified in the spec, there's
-                    // no need to turn that into a requirement to
-                    // find a var with an empty value.
-                    if let Some(value) = opt.get_value(None)
-                        && !value.is_empty()
-                    {
-                        requests.insert_or_merge_pinned(PinnedRequest::Var(
-                            opt.to_request(Some(value.as_str())),
-                        ))?;
-                    }
-                }
-            }
-        }
+        let requests =
+            build_requirements_from_build_options(self.ident(), &self.get_build_options())?;
         Ok(Cow::Owned(requests))
     }
 
